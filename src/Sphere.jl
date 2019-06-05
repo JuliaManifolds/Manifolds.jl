70939bd50368a784bf25b5811dd78f1a5b1953f9
Antoine Levitt <antoine.levitt@gmail.com>
2019-06-05 20:31:00 +0200

    forgot log!

--- src/Sphere.jl
+++ src/Sphere.jl
@@ -4,7 +4,8 @@ end
 
 project_tangent!(S::Sphere,w,x,v) = (w .= w - dot(x,v)*x)
 distance(S::Sphere,x,y) = norm(x-y) # FIXME
-exp!(S::Sphere,y,x,v) = retract!(S,y,x,v)
+exp!(S::Sphere,y,x,v) = retract!(S,y,x,v) # FIXME
+log!(S::Sphere,v,x,y) = project_tangent!(S,v,x,y-x) # FIXME
 dimension(S::Sphere) = sum(S.shape)
 random_point(S::Sphere) = (x = randn(S.shape); x / norm(x))
 random_tangent_vector(S::Sphere, x) = (v = randn(S.shape); project_tangent!(v,x,v))
