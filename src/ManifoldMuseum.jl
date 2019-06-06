ea095f85981eabddc1f4847d4b311f987d478605
Mateusz Baran <mateuszbaran89@gmail.com>
2019-06-06 19:37:38 +0200

    tests, some work on Sphere

--- src/ManifoldMuseum.jl
+++ src/ManifoldMuseum.jl
@@ -1,30 +1,39 @@
 module ManifoldMuseum
 
-export Manifold
+import Base: isapprox, exp, log
+import LinearAlgebra: dot, norm
+
 
 abstract type Manifold end
 
-retract!(M::Manifold, y, x, v) = exp!(M,y,x,v)
-retract(M::Manifold, x, v) = retract!(M,copy(x),x,v)
+isapprox(m::Manifold, x, y; kwargs...) = error("Not implemented")
+
+retract!(M::Manifold, y, x, v) = exp!(M, y, x, v)
+retract(M::Manifold, x, v) = retract!(M, copy(x), x, v)
 
 project_tangent!(M::Manifold, w, x, v) = error("Not implemented")
-project_tangent(M::Manifold, x, v) = project_tangent!(M,copy(x),x,v)
+project_tangent(M::Manifold, x, v) = project_tangent!(M, copy(x), x, v)
 
 distance(M::Manifold, x, y) = error("Not implemented")
 
-dot(M::Manifold, x, v, w) = dot(v,w)
+dot(M::Manifold, x, v, w) = dot(v, w)
 norm(M::Manifold, x, v) = sqrt(dot(M, x, v, v))
 
 exp!(M::Manifold, y, x, v) = error("Not implemented")
-exp(M::Manifold, x, v) = exp!(M,copy(x),x,v)
+exp(M::Manifold, x, v) = exp!(M,copy(x), x, v)
 
 log!(M::Manifold, v, x, y) = error("Not implemented")
-log(M::Manifold, x, y) = error("Not implemented")
+
+function log(M::Manifold, x, y)
+    v = zero_tangent_vector(M, x)
+    log!(M, v, x, y)
+    return v
+end
 
 dimension(M::Manifold) = error("Not implemented")
 
-vector_transport!(M::Manifold, vto, x, v, y) = project_tangent!(M,w,x,v)
-vector_transport(M::Manifold, x, v, y) = vector_transport!(M,copy(v),x,y,v)
+vector_transport!(M::Manifold, vto, x, v, y) = project_tangent!(M, vto, x, v)
+vector_transport(M::Manifold, x, v, y) = vector_transport!(M, copy(v), x, y, v)
 
 random_point(M::Manifold) = error("Not implemented")
 random_tangent_vector(M::Manifold, x) = error("Not implemented")
@@ -36,5 +45,21 @@ zero_tangent_vector!(M::Manifold, v, x) = log!(M, v, x, x)
 geodesic(M::Manifold, x, y, t) = exp(M, x, log(M, x, y), t)
 
 include("Sphere.jl")
+
+export Manifold
+export dimension,
+    distance,
+    dot,
+    exp,
+    exp!,
+    geodesic,
+    isapprox,
+    log,
+    log!,
+    norm,
+    typical_distance,
+    zero_tangent_vector,
+    zero_tangent_vector!
+
 
 end # module
--- src/Sphere.jl
+++ src/Sphere.jl
@@ -2,10 +2,37 @@ struct Sphere{T} <: Manifold where {T}
     shape::T
 end
 
-project_tangent!(S::Sphere,w,x,v) = (w .= w - dot(x,v)*x)
-distance(S::Sphere,x,y) = norm(x-y) # FIXME
-exp!(S::Sphere,y,x,v) = retract!(S,y,x,v) # FIXME
-log!(S::Sphere,v,x,y) = project_tangent!(S,v,x,y-x) # FIXME
+isapprox(S::Sphere, x, y; kwargs...) = isapprox(x, y; kwargs...)
+project_tangent!(S::Sphere, w, x, v) = (w .= w .- dot(x, v).*x)
+distance(S::Sphere, x, y) = acos(dot(x, y))
+
+function exp!(S::Sphere, y, x, v)
+    nv = norm(S, x, v)
+    if nv ≈ 0.0
+        y .= x
+    else
+        y .= cos(nv).*x .+ (sin(nv)/nv).*v
+    end
+    return y
+end
+
+function log!(S::Sphere, v, x, y)
+    θ = acos(dot(x, y))
+    if θ ≈ 0.0
+        zero_tangent_vector!(S, v, x)
+    else
+        v .= (θ/sin(θ)) .* (y .- cos(θ).*x)
+    end
+    return v
+end
+
 dimension(S::Sphere) = sum(S.shape)
 random_point(S::Sphere) = (x = randn(S.shape); x / norm(x))
-random_tangent_vector(S::Sphere, x) = (v = randn(S.shape); project_tangent!(v,x,v))
+
+function random_tangent_vector(S::Sphere, x)
+    v = randn(S.shape)
+    return project_tangent!(S, v, x, v)
+end
+
+zero_tangent_vector(S::Sphere, x) = zero(x)
+zero_tangent_vector!(S::Sphere, v, x) = (v .= zero(x))
