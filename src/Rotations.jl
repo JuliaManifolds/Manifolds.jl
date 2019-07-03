fe0692c06c836503329281635bca60808bb869ee
Seth Axen <seth.axen@gmail.com>
2019-07-03 01:33:30 -0700

    Add Special Orthogonal Group

--- src/Rotations.jl
+++ src/Rotations.jl
@@ -13,6 +13,25 @@ generates the $\mathrm{SO}(n) \subset \mathbb R^{n\times n}$
 struct Rotations{N} <: Manifold end
 Rotations(n::Int) = Rotations{n}()
 
+"""
+    SpecialOrthogonal{N} <: GroupManifold{Rotations{N},MultiplicationOperation}
+
+# Constructor
+
+    SpecialOrthogonal(n)
+"""
+const SpecialOrthogonal{N} = GroupManifold{Rotations{N},MultiplicationOperation}
+
+function SpecialOrthogonal(n)
+    return SpecialOrthogonal{n}(Rotations(n), MultiplicationOperation())
+end
+
+function show(io::IO, ::SpecialOrthogonal{N}) where {N}
+    print(io, "SpecialOrthogonal($(N))")
+end
+
+@traitimpl IsMatrixGroup{SpecialOrthogonal}
+
 @doc doc"""
     manifold_dimension(S::Rotations)
 
