af2b3483ec54d9983f764574093c4b76e20f2e4e
Mateusz Baran <mateuszbaran89@gmail.com>
2019-12-06 12:11:39 +0100

    modified exp! for LogCholeskyMetric

--- src/SymmetricPositiveDefinite.jl
+++ src/SymmetricPositiveDefinite.jl
@@ -214,7 +214,7 @@ denotes the lower triangular matrix with the diagonal multiplied by $\frac{1}{2}
 """
 function exp!(M::MetricManifold{SymmetricPositiveDefinite{N},LogCholeskyMetric}, y, x, v) where N
     (l,w) = spd_to_cholesky(x,v)
-    z = exp!(CholeskySpace{N}(),y,l,w)
+    z = exp(CholeskySpace{N}(),l,w)
     y .= z*z'
     return y
 end
