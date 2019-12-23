f5b6f41d50d59b1c33b51153919cafc88e05d5ac
Mateusz Baran <mateuszbaran89@gmail.com>
2019-12-23 18:38:26 +0100

    tests for complex Euclidean

--- test/euclidean.jl
+++ test/euclidean.jl
@@ -11,7 +11,7 @@ include("utils.jl")
     @test log_local_metric_density(EM,x) == zero(eltype(x))
     @test project_point!(E,x) == x
     @test manifold_dimension(Ec) == 2*manifold_dimension(E)
-    
+
     manifolds = [ E, EM, Ec ]
     types = [Vector{Float64},
              SizedVector{3, Float64},
@@ -22,6 +22,16 @@ include("utils.jl")
              Vector{Double64},
              MVector{3, Double64},
              SizedVector{3, Double64}]
+
+    types_complex = [Vector{ComplexF64},
+                     SizedVector{3, ComplexF64},
+                     MVector{3, ComplexF64},
+                     Vector{ComplexF32},
+                     SizedVector{3, ComplexF32},
+                     MVector{3, ComplexF32},
+                     Vector{ComplexDF64},
+                     MVector{3, ComplexDF64},
+                     SizedVector{3, ComplexDF64}]
     for M in manifolds
         for T in types
             @testset "$M Type $T" begin
@@ -40,4 +50,18 @@ include("utils.jl")
             end
         end
     end
+    for T in types_complex
+        @testset "Complex Euclidean, type $T" begin
+            pts = [convert(T, [1.0im, -1.0im, 1.0]),
+                   convert(T, [0.0, 1.0, 1.0im]),
+                   convert(T, [0.0, 0.0, 1.0])]
+            test_manifold(Ec,
+                          pts,
+                          test_reverse_diff = isa(T, Vector),
+                          test_project_tangent = true,
+                          test_musical_isomorphisms = true,
+                          test_vector_transport = true)
+        end
+    end
+
 end
