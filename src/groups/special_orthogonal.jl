600e1edab91472a120bcf701a5cf62ea42805237
Seth Axen <seth.axen@gmail.com>
2020-01-04 01:45:29 -0800

    Reformat

--- src/groups/special_orthogonal.jl
+++ src/groups/special_orthogonal.jl
@@ -17,13 +17,13 @@ function inv!(G::SpecialOrthogonal, y, x)
     copyto!(y, inv(G, x))
     return y
 end
-function inv!(G::AG, y, e::Identity{AG}) where AG<:SpecialOrthogonal
+function inv!(G::AG, y, e::Identity{AG}) where {AG<:SpecialOrthogonal}
     identity!(G, y, e)
     return y
 end
 
 inv(G::SpecialOrthogonal, x) = transpose(x)
-inv(::AG, e::Identity{AG}) where AG<:SpecialOrthogonal = e
+inv(::AG, e::Identity{AG}) where {AG<:SpecialOrthogonal} = e
 
 function show(io::IO, ::SpecialOrthogonal{N}) where {N}
     print(io, "SpecialOrthogonal($(N))")
