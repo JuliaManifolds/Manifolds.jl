74728c2a0b4e8024ff2006b82c9280d54b39e717
Seth Axen <seth.axen@gmail.com>
2020-01-04 01:45:53 -0800

    Add diff implementations for Rotations

--- src/groups/special_orthogonal.jl
+++ src/groups/special_orthogonal.jl
@@ -28,3 +28,33 @@ inv(::AG, e::Identity{AG}) where {AG<:SpecialOrthogonal} = e
 function show(io::IO, ::SpecialOrthogonal{N}) where {N}
     print(io, "SpecialOrthogonal($(N))")
 end
+
+translate_diff(::SpecialOrthogonal, x, y, v, ::LeftAction) = v
+translate_diff(::SpecialOrthogonal, x, y, v, ::RightAction) = transpose(x) * v * x
+
+function translate_diff!(
+    G::SpecialOrthogonal,
+    vout,
+    x,
+    y,
+    v,
+    conv::ActionDirection = LeftAction(),
+)
+    copyto!(vout, translate_diff(G, x, y, v, conv))
+    return vout
+end
+
+inverse_translate_diff(::SpecialOrthogonal, x, y, v, ::LeftAction) = v
+inverse_translate_diff(::SpecialOrthogonal, x, y, v, ::RightAction) = x * v * transpose(x)
+
+function inverse_translate_diff!(
+    G::SpecialOrthogonal,
+    vout,
+    x,
+    y,
+    v,
+    conv::ActionDirection = LeftAction(),
+)
+    copyto!(vout, inverse_translate_diff(G, x, y, v, conv))
+    return vout
+end
