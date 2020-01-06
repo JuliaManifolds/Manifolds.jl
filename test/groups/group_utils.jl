b607ca608e3e2126bc27921951bb7fa53a36b088
Seth Axen <seth.axen@gmail.com>
2020-01-05 22:27:18 -0800

    Expand action test suite

--- test/groups/group_utils.jl
+++ test/groups/group_utils.jl
@@ -173,11 +173,16 @@ function test_group(
     end
 end
 
-
 """
-    test_action(A::AbstractGroupAction,
+    test_action(
+        A::AbstractGroupAction,
         a_pts::AbstractVector,
-        m_pts::AbstractVector)
+        m_pts::AbstractVector;
+        atol_inv = 1e-10,
+        test_optimal_alignment = false,
+        test_mutating = true,
+        test_diff = false,
+    )
 
 Tests general properties of the action `A`, given at least three different points
 that lie on it (contained in `a_pts`) and three different point that lie
@@ -186,154 +191,186 @@ on the manifold it acts upon (contained in `m_pts`).
 function test_action(
         A::AbstractGroupAction,
         a_pts::AbstractVector,
-        m_pts::AbstractVector;
+        m_pts::AbstractVector,
+        v_pts = [];
         atol_inv = 1e-10,
-        test_optimal_alignment = false
+        test_optimal_alignment = false,
+        test_mutating = true,
+        test_diff = false,
     )
 
     G = base_group(A)
-    GM = base_manifold(G)
     M = g_manifold(A)
     e = Identity(G)
 
-    @testset "Action direction" begin
-        if isa(A, AbstractGroupAction{LeftAction})
-            @test isa(switch_direction(A), AbstractGroupAction{RightAction})
-        else
-            @test isa(switch_direction(A), AbstractGroupAction{LeftAction})
-        end
-    end
-
-    @testset "Type calculation and adding identity element" begin
-        for ap in a_pts
-            e_ap = e(ap)
-            @test isapprox(G, e_ap, identity(G, ap))
-            @test isapprox(G, compose(G, e, ap), ap)
-            @test isapprox(G, compose(G, e_ap, ap), ap)
-            @test isapprox(G, compose(G, ap, e), ap)
-            @test isapprox(G, compose(G, ap, e_ap), ap)
-            inv_ap = inv(G, ap)
-            @test isapprox(G, compose(G, ap, inv_ap), e_ap; atol = atol_inv)
-            @test isapprox(G, compose(G, inv_ap, ap), e_ap; atol = atol_inv)
-            ap2 = similar(e_ap)
-            identity!(G, ap2, e_ap)
-            @test isapprox(G, ap2, e_ap)
+    @testset "Basic action properties" begin
+        @testset "Closed" begin
+            @testset "over actions" begin
+                for a1 in a_pts, a2 in a_pts
+                    a3 = compose(A, a1, a2)
+                    @test is_manifold_point(G, a3)
+                end
+            end
+            @testset "over g-manifold" begin
+                for a in a_pts, m in m_pts
+                    @test is_manifold_point(M, apply(A, a, m))
+                    @test is_manifold_point(M, inverse_apply(A, a, m))
+                end
+            end
         end
 
-        @test inv(G, e) == e
-        b = similar(a_pts[1])
-    end
+        @testset "Associative" begin
+            a12 = compose(A, a_pts[1], a_pts[2])
+            a23 = compose(A, a_pts[2], a_pts[3])
 
-    @testset "Associativity" begin
-        N = length(a_pts)
-        for i in 1:N
-            assoc_l = compose(G, compose(G, a_pts[i], a_pts[mod1(i+1, N)]), a_pts[mod1(i+2, N)])
-            assoc_r = compose(G, a_pts[i], compose(G, a_pts[mod1(i+1, N)], a_pts[mod1(i+2, N)]))
-            @test isapprox(G, assoc_l, assoc_r)
+            @testset "over compose" begin
+                a12_a3 = compose(A, a12, a_pts[3])
+                a1_a23 = compose(A, a_pts[1], a23)
+                @test isapprox(G, a12_a3, a1_a23)
+            end
+
+            @testset "over apply" begin
+                for m in m_pts
+                    a12_a3_m = apply(A, a12, apply(A, a_pts[3], m))
+                    a1_a23_m = apply(A, a_pts[1], apply(A, a23, m))
+                    @test isapprox(M, a12_a3_m, a1_a23_m)
+                end
+            end
+
+            test_mutating && @testset "mutating" begin
+                a12, a23, a12_3, a1_23 = similar.(repeat([a_pts[1]], 4))
+                @test compose!(A, a12, a_pts[1], a_pts[2]) === a12
+                @test compose!(A, a23, a_pts[2], a_pts[3]) === a23
+                @test compose!(A, a12_3, a12, a_pts[3]) === a12_3
+                @test compose!(A, a1_23, a_pts[1], a23) === a1_23
+                @test isapprox(G, a12_3, a1_23)
+
+                for m in m_pts
+                    a12_a3_m, a1_a23_m = similar(m), similar(m)
+                    @test apply!(A, a12_a3_m, a12, apply(A, a_pts[3], m)) === a12_a3_m
+                    @test apply!(A, a1_a23_m, a_pts[1], apply(A, a23, m)) === a1_a23_m
+                    @test isapprox(M, a12_a3_m, a1_a23_m)
+                end
+            end
         end
-    end
 
-    @testset "Compatibility" begin
-        N = length(a_pts)
-        for p in m_pts
-            for i in 1:N
-                aip = apply(A, a_pts[mod1(i+1, N)], p)
-                acg = compose(G, a_pts[i], a_pts[mod1(i+1, N)])
-                @test isapprox(M, apply(A, acg, p), apply(A, a_pts[i], aip))
+        @testset "Identity" begin
+            @test compose(A, e, e) === e
+
+            for a in a_pts
+                @test isapprox(G, compose(A, a, e), a)
+                @test isapprox(G, compose(A, e, a), a)
+
+                ge = identity(G, a)
+                @test isapprox(G, compose(A, a, ge), a)
+                @test isapprox(G, compose(A, ge, a), a)
+
+                for m in m_pts
+                    @test isapprox(M, apply(A, e, m), m)
+                    @test isapprox(M, apply(A, ge, m), m)
+                    @test isapprox(M, inverse_apply(A, e, m), m)
+                    @test isapprox(M, inverse_apply(A, ge, m), m)
+                end
+            end
+
+            test_mutating && @testset "mutating" begin
+                for a in a_pts
+                    h = similar(a)
+                    @test compose!(A, h, a, e) === h
+                    @test isapprox(G, h, a)
+                    h = similar(a)
+                    @test compose!(A, h, e, a) === h
+                    @test isapprox(G, h, a)
+
+                    ge = identity(G, a)
+                    @test isapprox(G, compose(A, a, ge), a)
+                    @test isapprox(G, compose(A, ge, a), a)
+
+                    ge = identity(G, a)
+                    @test compose!(A, ge, e, e) === ge
+                    @test isapprox(G, ge, e)
+
+                    for m in m_pts
+                        em = similar(m)
+                        @test apply!(A, em, e, m) === em
+                        @test isapprox(M, em, m)
+                        em = similar(m)
+                        @test apply!(A, em, ge, m) === em
+                        @test isapprox(M, em, m)
+                        em = similar(m)
+                        @test inverse_apply!(A, em, e, m) === em
+                        @test isapprox(M, em, m)
+                        em = similar(m)
+                        @test inverse_apply!(A, em, ge, m) === em
+                        @test isapprox(M, em, m)
+                    end
+                end
+            end
+        end
+
+        @testset "Inverse" begin
+            for a in a_pts
+                ainv = inv(G, a)
+                @test isapprox(G, compose(A, a, ainv), e)
+                @test isapprox(G, compose(A, ainv, a), e)
+                @test isapprox(G, e, compose(A, a, ainv))
+                @test isapprox(G, e, compose(A, ainv, a))
+
+                for m in m_pts
+                    @test isapprox(M, apply(A, a, m), inverse_apply(A, ainv, m))
+                    @test isapprox(M, apply(A, ainv, m), inverse_apply(A, a, m))
+                end
             end
         end
     end
 
-    @testset "Mutable apply!" begin
-        N = length(a_pts)
-        for p in m_pts
-            for i in 1:N
-                aip = similar(p)
-                apply!(A, aip, a_pts[i], p)
-                @test isapprox(M, apply(A, a_pts[i], p), aip)
+    test_diff && @testset "apply differential" begin
+        for (m, v) in zip(m_pts, v_pts)
+            for a in a_pts
+                am, av = apply(A, a, m), apply_diff(A, a, m, v)
+                ainvm, ainvv = inverse_apply(A, a, m), inverse_apply_diff(A, a, m, v)
+                @test is_tangent_vector(M, am, av)
+                @test is_tangent_vector(M, ainvm, ainvv)
             end
+
+            a12 = compose(A, a_pts[1], a_pts[2])
+            a2m = apply(A, a_pts[2], m)
+            a12v = apply_diff(A, a12, m, v)
+            a2v = apply_diff(A, a_pts[2], m, v)
+            @test apply_diff(A, a_pts[1], a2m, a2v) ≈ a12v
+
+            @test apply_diff(A, e, m, v) ≈ v
+            @test inverse_apply_diff(A, e, m, v) ≈ v
         end
-    end
 
-    @testset "Mutable inv!" begin
-        for ap in a_pts
-            ai = similar(ap)
-            inv!(G, ai, ap)
-            @test isapprox(G, compose(G, ai, ap), e(ap))
-            @test isapprox(G, compose(G, ap, ai), e(ap))
-        end
-    end
+        test_mutating && @testset "mutating" begin
+            for (m, v) in zip(m_pts, v_pts)
+                for a in a_pts
+                    am = apply(A, a, m)
+                    av = similar(v)
+                    @test apply_diff!(A, av, a, m, v) === av
+                    ainvm = inverse_apply(A, a, m)
+                    ainvv = similar(v)
+                    @test inverse_apply_diff!(A, ainvv, a, m, v) === ainvv
+                    @test is_tangent_vector(M, am, av)
+                    @test is_tangent_vector(M, ainvm, ainvv)
+                end
 
-    @testset "Translate vs compose" begin
-        N = length(a_pts)
-        for i in 1:N
-            ai = a_pts[i]
-            aip = a_pts[mod1(i+1, N)]
-            @test isapprox(G, translate(G, ai, aip), compose(G, ai, aip))
-            @test isapprox(G, translate(G, ai, aip, LeftAction()), compose(G, ai, aip))
-            @test isapprox(G, translate(G, aip, ai, RightAction()), compose(G, ai, aip))
-        end
-    end
+                a12 = compose(A, a_pts[1], a_pts[2])
+                a2m = apply(A, a_pts[2], m)
+                a12v, a2v, a1_a2v = similar(v), similar(v), similar(v)
+                @test apply_diff!(A, a12v, a12, m, v) === a12v
+                @test apply_diff!(A, a2v, a_pts[2], m, v) === a2v
+                @test apply_diff!(A, a1_a2v, a_pts[1], a2m, a2v) === a1_a2v
+                @test a1_a2v ≈ a12v
 
-    @testset "Translate mutation" begin
-        N = length(a_pts)
-        for i in 1:N
-            ai = a_pts[i]
-            aip = a_pts[mod1(i+1, N)]
-            as = similar(ai)
-            translate!(G, as, ai, aip)
-            @test isapprox(G, as, translate(G, ai, aip))
-            translate!(G, as, ai, aip, LeftAction())
-            @test isapprox(G, as, translate(G, ai, aip, LeftAction()))
-            translate!(G, as, ai, aip, RightAction())
-            @test isapprox(G, as, translate(G, ai, aip, RightAction()))
-        end
-    end
-
-    @testset "translate_diff" begin
-        tv = log(GM, a_pts[1], a_pts[2])
-        tvd = translate_diff(G, a_pts[2], a_pts[1], tv)
-        @test is_tangent_vector(G, compose(G, a_pts[1], a_pts[2]), tvd)
-        tvdi = inverse_translate_diff(G, inv(G, a_pts[2]), a_pts[1], tv)
-        @test isapprox(G, a_pts[1], tvd, tvdi)
-    end
-
-    @testset "mutating translate_diff!" begin
-        tv = log(GM, a_pts[1], a_pts[2])
-        tvd = similar(tv)
-        translate_diff!(G, tvd, a_pts[2], a_pts[1], tv)
-        @test isapprox(GM, a_pts[1], tvd, translate_diff(G, a_pts[2], a_pts[1], tv))
-
-        tvdi = similar(tv)
-        inverse_translate_diff!(G, tvdi, inv(G, a_pts[2]), a_pts[1], tv)
-        @test isapprox(GM, a_pts[1], tvd, tvdi)
-    end
-
-    @testset "Action of group identity" begin
-        e_ap = e(a_pts[1])
-        for p in m_pts
-            @test isapprox(M, p, apply(A, e, p))
-            @test isapprox(M, p, apply(A, e_ap, p))
-        end
-    end
-
-    @testset "Action composition" begin
-        a12 = compose(A, a_pts[1], a_pts[2])
-        b = similar(a_pts[1])
-        compose!(A, b, a_pts[1], a_pts[2])
-        @test isapprox(G, a12, b)
-
-        if isa(A, AbstractGroupAction{LeftAction})
-            x_a = apply(A, a_pts[1], apply(A, a_pts[2], m_pts[1]))
-            x_b = apply(A, a12, m_pts[1])
-            @test isapprox(M, x_a, x_b)
-        elseif isa(A, AbstractGroupAction{RightAction})
-            x_a = apply(A, a_pts[2], apply(A, a_pts[1], m_pts[1]))
-            x_b = apply(A, a12, m_pts[1])
-            @test isapprox(M, x_a, x_b)
-        else
-            # most likely a bug in testset
-            @test false
+                ev = similar(v)
+                @test apply_diff!(A, ev, e, m, v) === ev
+                @test ev ≈ v
+                ev = similar(v)
+                @test inverse_apply_diff!(A, ev, e, m, v) === ev
+                @test ev ≈ v
+            end
         end
     end
 
