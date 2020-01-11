e9d450f0c1bd5b377db980920c8aa29de0aed5e6
Seth Axen <seth.axen@gmail.com>
2020-01-10 16:26:28 -0800

    Use size_to_tuple everywhere

--- src/manifolds/Euclidean.jl
+++ src/manifolds/Euclidean.jl
@@ -237,7 +237,7 @@ end
 Return the array dimensions required to represent an element on the
 [`Euclidean`](@ref) `M`, i.e. the vector of all array dimensions.
 """
-Base.@pure representation_size(::Euclidean{N}) where {N} = tuple(N.parameters...)
+representation_size(::Euclidean{N}) where {N} = size_to_tuple(N)
 
 """
     sharp(M::Euclidean, x, w)
