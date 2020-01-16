f00026a74f25938266aa87a6306cac898419ad39
Ronny Bergmann <git@ronnybergmann.net>
2020-01-16 08:51:50 +0100

    docufy.

--- src/manifolds/GraphManifold.jl
+++ src/manifolds/GraphManifold.jl
@@ -1,6 +1,7 @@
 
 using LightGraphs
 import LightGraphs: AbstractSimpleGraph
+import SimpleWeightedGraph: SimpleWeightedGraph
 
 @doc doc"""
     GraphManifoldType
@@ -25,17 +26,14 @@ A type for a [`GraphManifold`](@ref) where the data is given on the vertices.
 struct VertexManifold <: GraphManifoldType end
 
 @doc doc"""
-    GraphManifold{G, M, T} <: AbstractPowerManifold
+    GraphManifold{G, M, T} <: AbstractPowerManifold{M}
 
-Build a manifold, that is a [`PowerManifold`](@ref) either on the
-edges or vertices of a graph `G` depending on the [`GraphManifoldType`](@ref) `T`.
-
+Build a manifold, that is a [`PowerManifold`](@ref) of the [`Manifold`](@ref) `M` either on the edges or vertices
+of a graph `G` depending on the [`GraphManifoldType`](@ref) `T`.
 
 # Fields
 * `G` is an `AbstractSimpleGraph`
 * `M` is a [`Manifold`](@ref)
-* `T` is a [`GraphManifoldType`](@ref), i.e. whether data is given on vertices
-or edges.
 """
 struct GraphManifold{G<:AbstractSimpleGraph, TM, T<:GraphManifoldType} <: AbstractPowerManifold{TM}
     graph::G
@@ -43,12 +41,16 @@ struct GraphManifold{G<:AbstractSimpleGraph, TM, T<:GraphManifoldType} <: Abstra
 end
 GraphManifold(g::G,M::TM,::VertexManifold) where {G<:AbstractSimpleGraph, TM <: Manifold} = GraphManifold{G,TM,VertexManifold}(g,M)
 GraphManifold(g::G,M::TM,::EdgeManifold) where {G<:AbstractSimpleGraph, TM <: Manifold} = GraphManifold{G,TM,EdgeManifold}(g,M)
+
 @doc doc"""
     check_manifold_point(M::GraphManifold,x)
 
-check whether `x` is a valid point on the [`GraphManifold`](@ref)
+check whether `x` is a valid point on the [`GraphManifold`](@ref), i.e. its
+length equals the number of vertices (for [`VertexManifold`](@ref)s) or
+the number of edges (for [`EdgeManifold`](@ref)s) and that each element of `x`
+passes the [`check_manifold_point`](@ref) test for the base manifold `M.manifold`.
 """
-check_manifold_point(::GraphManifold,::Any)
+check_manifold_point(::GraphManifold,::Any...)
 function check_manifold_point(
     M::GraphManifold{G,TM,VertexManifold},
     x;
@@ -68,6 +70,17 @@ function check_manifold_point(
     return check_manifold_point(PowerManifold(M.manifold,nv(M.graph)), x; kwargs...)
 end
 
+@doc doc"""
+    check_tangent_vector(M::GraphManifold,x,v)
+
+check whether `x` is a valid point on the [`GraphManifold`](@ref), and
+`v` it from its tangent space, i.e. its
+length equals the number of vertices (for [`VertexManifold`](@ref)s) or
+the number of edges (for [`EdgeManifold`](@ref)s) and that each element of `v`
+together with its corresponding einty of `x` passes the
+[`check_tangent_vector`](@ref) test for the base manifold `M.manifold`.
+"""
+check_tangent_vector(::GraphManifold,::Any...)
 function check_tangent_vector(
     M::GraphManifold{G, TM, VertexManifold},
     x,
@@ -95,7 +108,6 @@ function check_tangent_vector(
     return check_tangent_vector(PowerManifold(M.manifold,ne(M.graph)), x, v; kwargs...)
 end
 
-
 function get_iterator(M::GraphManifold{<: AbstractSimpleGraph, <:Manifold, EdgeManifold})
     return 1:ne(M.graph)
 end
@@ -104,10 +116,13 @@ function get_iterator(M::GraphManifold{<: AbstractSimpleGraph, <:Manifold, Verte
 end
 
 @doc doc"""
-    incident_log(M, x)
+    incident_log(M::GraphManifold, x)
 
 return the tangent vector on the (vertex) [`GraphManifold`](@ref), where at
-each node the sum of the [`log`](@ref)s to incident nodes is summed.
+each node the sum of the [`log`](@ref)s to incident nodes is computed.
+
+If the internal graph is a `SimpleWeightedGraph` the weighted sum of the
+tangent vectors is computed.
 """
 function incident_log(M::GraphManifold{G, TM, VertexManifold}, x) where {G <: AbstractSimpleGraph, TM<:Manifold}
     v = zero_tangent_vector(M,x)
@@ -119,17 +134,53 @@ function incident_log!(M::GraphManifold{G, TM, VertexManifold}, v, x) where {G <
     end
     return v;
 end
+function incident_log!(M::GraphManifold{G, TM, VertexManifold}, v, x) where {G <: SimpleWeightedGraph, TM<:Manifold}
+    for e in edges(M.graph)
+        v[src(e)] += M.graph.weight[src(e),dst(e)]*log(M.manifold, x[src(e)], x[dst(e)])
+    end
+    return v;
+end
 
+@doc doc"""
+    manifold_dimension(N::GraphManifold{G,M,VertexManifold})
+
+returns the manifold dimension of the [`GraphManifold`](@ref) `N` on the vertices of
+a graph $G=(V,E)$, i.e.
+````math
+d_{\mathcal N} = \lvert V \rVert d_{\mathcal M}.
+````
+"""
 function manifold_dimension(M::GraphManifold{<: AbstractSimpleGraph,<: Manifold, VertexManifold})
     return manifold_dimension(M.manifold)*nv(M.graph)
 end
+@doc doc"""
+    manifold_dimension(N::GraphManifold{G,M,EdgeManifold})
+
+returns the manifold dimension of the [`GraphManifold`](@ref) `N` on the edges of
+a graph $G=(V,E)$, i.e.
+````math
+d_{\mathcal N} = \lvert E \rVert d_{\mathcal M}.
+````
+"""
 function manifold_dimension(M::GraphManifold{<: AbstractSimpleGraph, <: Manifold, EdgeManifold})
     return manifold_dimension(M.manifold)*ne(M.graph)
 end
 
+@doc doc"""
+    representationsize(N::GraphManifold{G,M,VertexManifold})
+
+returns the representation size of a point on the [`GraphManifold`](@ref) on the vertices of $G=(V,E),
+which is the representation size of `M` with $\lvert V \rvert$ added.
+"""
 function representation_size(M::GraphManifold{<: AbstractSimpleGraph,<: Manifold, VertexManifold})
     return (representation_size(M.manifold)..., nv(M.graph))
 end
+@doc doc"""
+    representationsize(N::GraphManifold{G,M,EdgeManifold})
+
+returns the representation size of a point on the [`GraphManifold`](@ref) on the vertices of $G=(V,E),
+which is the representation size of `M` with $\lvert E \rvert$ added.
+"""
 function representation_size(M::GraphManifold{<: AbstractSimpleGraph, <: Manifold, EdgeManifold})
     return (representation_size(M.manifold)..., ne(M.graph))
 end
