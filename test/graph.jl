510ea4e2e37a7d2a7ddfef74ecf94d1f43f54e81
Ronny Bergmann <git@ronnybergmann.net>
2020-01-16 10:26:34 +0100

    correct number of vertices in manifoldpoints.

--- test/graph.jl
+++ test/graph.jl
@@ -10,9 +10,9 @@ using LightGraphs, SimpleWeightedGraphs
         N = GraphManifold(G,M,VertexManifold())
         @test manifold_dimension(N) == manifold_dimension(M)*nv(G)
         @test manifold_dimension(GraphManifold(G,M,EdgeManifold())) == manifold_dimension(M)*ne(G)
-        x = [[1,2], [2,3]]
-        y = [[4,5], [6,7]]
-        z = [[6,5], [4,3]]
+        x = [[1,2], [2,3], [2,3]]
+        y = [[4,5], [6,7], [8,9]]
+        z = [[6,5], [4,3], [2,8]]
         pts = [x,y,z]
         test_manifold(N, pts)
     end
