ea01e7406c3c7ed3d3a68a0040dd425166c6f09f
Seth Axen <seth.axen@gmail.com>
2020-01-21 23:38:02 -0800

    Add more SE(n) tests

--- test/groups/special_euclidean.jl
+++ test/groups/special_euclidean.jl
@@ -17,60 +17,81 @@ function _to_affine(p, is_vector = false)
 end
 
 @testset "Special Euclidean group" begin
-    G = SpecialEuclidean(3)
-    @test repr(G) == "SpecialEuclidean(3)"
-    M = base_manifold(G)
-    @test M === TranslationGroup(3) × SpecialOrthogonal(3)
-    Rn = Rotations(3)
-    x = Matrix(I, 3, 3)
+    @testset "SpecialEuclidean($n)" for n in (2, 3, 4)
+        G = SpecialEuclidean(n)
+        @test repr(G) == "SpecialEuclidean($n)"
+        M = base_manifold(G)
+        @test M === TranslationGroup(n) × SpecialOrthogonal(n)
+        Rn = Rotations(n)
+        x = Matrix(I, n, n)
 
-    t = Vector{Float64}.([1:3, 2:4, 4:6])
-    ω = [[1.0, 2.0, 3.0], [3.0, 2.0, 1.0], [1.0, 3.0, 2.0]]
-    tuple_pts = [(ti, exp(Rn, x, hat(Rn, x, ωi))) for (ti, ωi) in zip(t, ω)]
-    tuple_v = ([-1.0, 2.0, 1.0], hat(Rn, x, [1.0, 0.5, -0.5]))
-
-    @testset "product point" begin
-        reshapers = (Manifolds.ArrayReshaper(), Manifolds.StaticReshaper())
-        for reshaper in reshapers
-            shape_se = Manifolds.ShapeSpecification(reshaper, M.manifolds...)
-            pts = [Manifolds.prod_point(shape_se, tp...) for tp in tuple_pts]
-            v_pts = [Manifolds.prod_point(shape_se, tuple_v...)]
-
-            g1, g2 = pts[1:2]
-            t1, R1 = g1.parts
-            t2, R2 = g2.parts
-            g1g2 = Manifolds.prod_point(shape_se, R1 * t2 + t1, R1 * R2)
-            @test compose(G, g1, g2) ≈ g1g2
-            @test _to_affine(g1g2) ≈ _to_affine(g1) * _to_affine(g2)
-
-            w = translate_diff(G, pts[1], Identity(G), v_pts[1])
-            w2 = similar(w)
-            w2.parts[1] .= w.parts[1]
-            w2.parts[2] .= pts[1].parts[2] * w.parts[2]
-            @test _to_affine(w2, true) ≈ _to_affine(pts[1]) * _to_affine(v_pts[1], true)
-
-            test_group(G, pts, v_pts; test_diff = true, diff_convs = [(), (LeftAction(),)])
+        if n == 2
+            t = Vector{Float64}.([1:2, 2:3, 3:4])
+            ω = [[1.0, 2.0], [2.0, 1.0], [1.0, 3.0]]
+            tuple_pts = [(ti, exp(Rn, x, hat(Rn, x, ωi))) for (ti, ωi) in zip(t, ω)]
+            tuple_v = ([-1.0, 2.0], hat(Rn, x, [1.0, -0.5]))
+        elseif n == 3
+            t = Vector{Float64}.([1:3, 2:4, 4:6])
+            ω = [[1.0, 2.0, 3.0], [3.0, 2.0, 1.0], [1.0, 3.0, 2.0]]
+            tuple_pts = [(ti, exp(Rn, x, hat(Rn, x, ωi))) for (ti, ωi) in zip(t, ω)]
+            tuple_v = ([-1.0, 2.0, 1.0], hat(Rn, x, [1.0, 0.5, -0.5]))
+        else # n == 4
+            t = Vector{Float64}.([1:4, 2:5, 3:6])
+            ω = [
+                [1.0, 2.0, 3.0, 1.0, 2.0, 3.0],
+                [3.0, 2.0, 1.0, 1.0, 2.0, 3.0],
+                [1.0, 3.0, 2.0, 1.0, 2.0, 3.0],
+            ]
+            tuple_pts = [(ti, exp(Rn, x, hat(Rn, x, ωi))) for (ti, ωi) in zip(t, ω)]
+            tuple_v = (
+                [-1.0, 2.0, 1.0, 3.0],
+                hat(Rn, x, [1.0, 0.5, -0.5, 0.0, 2.0, 1.0]),
+            )
         end
-    end
 
-    @testset "affine matrix" begin
-        shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
-        pts = [_to_affine(Manifolds.prod_point(shape_se, tp...)) for tp in tuple_pts]
-        v_pts = [_to_affine(Manifolds.prod_point(shape_se, tuple_v...), true)]
-        test_group(G, pts, v_pts; test_diff = true, diff_convs = [(), (LeftAction(),)])
-    end
+        @testset "product point" begin
+            reshapers = (Manifolds.ArrayReshaper(), Manifolds.StaticReshaper())
+            for reshaper in reshapers
+                shape_se = Manifolds.ShapeSpecification(reshaper, M.manifolds...)
+                pts = [Manifolds.prod_point(shape_se, tp...) for tp in tuple_pts]
+                v_pts = [Manifolds.prod_point(shape_se, tuple_v...)]
 
-    @testset "hat/vee" begin
-        shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
-        x = Manifolds.prod_point(shape_se, tuple_pts[1]...)
-        V = Manifolds.prod_point(shape_se, tuple_v...)
-        vexp = [V.parts[1]; vee(Rn, x.parts[2], V.parts[2])]
-        v = vee(G, x, V)
-        @test v ≈ vexp
-        @test hat(G, x, v) ≈ V
+                g1, g2 = pts[1:2]
+                t1, R1 = g1.parts
+                t2, R2 = g2.parts
+                g1g2 = Manifolds.prod_point(shape_se, R1 * t2 + t1, R1 * R2)
+                @test isapprox(G, compose(G, g1, g2), g1g2)
+                @test _to_affine(g1g2) ≈ _to_affine(g1) * _to_affine(g2)
 
-        v = vee(G, _to_affine(x), _to_affine(V, true))
-        @test v ≈ vexp
-        @test hat(G, _to_affine(x), v) ≈ _to_affine(V, true)
+                w = translate_diff(G, pts[1], Identity(G), v_pts[1])
+                w2 = similar(w)
+                w2.parts[1] .= w.parts[1]
+                w2.parts[2] .= pts[1].parts[2] * w.parts[2]
+                @test _to_affine(w2, true) ≈ _to_affine(pts[1]) * _to_affine(v_pts[1], true)
+
+                test_group(G, pts, v_pts, v_pts; test_diff = true, diff_convs = [(), (LeftAction(),)])
+            end
+        end
+
+        @testset "affine matrix" begin
+            shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
+            pts = [_to_affine(Manifolds.prod_point(shape_se, tp...)) for tp in tuple_pts]
+            v_pts = [_to_affine(Manifolds.prod_point(shape_se, tuple_v...), true)]
+            test_group(G, pts, v_pts, v_pts; test_diff = true, diff_convs = [(), (LeftAction(),)])
+        end
+
+        @testset "hat/vee" begin
+            shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
+            x = Manifolds.prod_point(shape_se, tuple_pts[1]...)
+            V = Manifolds.prod_point(shape_se, tuple_v...)
+            vexp = [V.parts[1]; vee(Rn, x.parts[2], V.parts[2])]
+            v = vee(G, x, V)
+            @test v ≈ vexp
+            @test hat(G, x, v) ≈ V
+
+            v = vee(G, _to_affine(x), _to_affine(V, true))
+            @test v ≈ vexp
+            @test hat(G, _to_affine(x), v) ≈ _to_affine(V, true)
+        end
     end
 end
