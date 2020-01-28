1167e5a20f91d6abb86af9341afdf8dc60be4da3
Manuel Weiss <manuel.weiss@s2016.tu-chemnitz.de>
2020-01-28 16:02:23 +0100

    adds check tangent vector to GeneralizedStiefel

--- src/manifolds/GeneralizedStiefel.jl
+++ src/manifolds/GeneralizedStiefel.jl
@@ -14,7 +14,7 @@ $I_n \in \mathbb R^{n\times n}$ denotes the $k \times k$ identity matrix.
 
 In the case $B=I_k$ one gets the usual [`Stiefel`](@ref) manifold.
 
-The tangent space at a point $x\in\mathcal M=St(n,k,B)$ is given by
+The tangent space at a point $x\in\mathcal M=\operatorname{St(n,k,B)}$ is given by
 
 ````math
 T_x\mathcal M = \{ v \in \mathbb{F}^{n\times k} : x^{\mathrm{H}}Bv + v^{\mathrm{H}}Bx=0_n\},
@@ -23,13 +23,22 @@ T_x\mathcal M = \{ v \in \mathbb{F}^{n\times k} : x^{\mathrm{H}}Bv + v^{\mathrm{
 # Constructor
     Stiefel(n,k,F=ℝ,B=I_k)
 
-Generate the (real-valued) Generalized Stiefel manifold of $n\times k$ dimensional orthonormal matrices.
+Generate the (real-valued) Generalized Stiefel manifold of $n\times k$ dimensional orthonormal matrices with scalar product `B`.
 """
 struct GeneralizedStiefel{n,k,F,TB<:AbstractMatrix} <: Manifold 
     B::TB
 end
 
-GeneralizedStiefel(n::Int, k::Int, F::AbstractNumbers = ℝ, B::AbstractMatrix = Matrix{Float64}(I,k,k)) = GeneralizedStiefel{n,k,F}(B)
+GeneralizedStiefel(n::Int, k::Int, F::AbstractNumbers = ℝ, B::AbstractMatrix = Matrix{Float64}(I,k,k)) = GeneralizedStiefel{n,k,F,typeof(B)}(B)
+
+@doc doc"""
+    representation_size(M::GeneralizedStiefel)
+
+Returns the representation size of the [`Stiefel`](@ref) `M`=$\operatorname{St}(n,k,B)$,
+i.e. `(n,k)`, which is the matrix dimensions.
+"""
+@generated representation_size(::GeneralizedStiefel{n,k}) where {n,k} = (n, k)
+
 
 @doc doc"""
     check_manifold_point(M::GeneralizedStiefel, x; kwargs...)
@@ -67,10 +76,41 @@ function check_manifold_point(M::GeneralizedStiefel{n,k,T}, x; kwargs...) where 
     end
 end
 
-@doc doc"""
-    representation_size(M::GeneralizedStiefel)
 
-Returns the representation size of the [`Stiefel`](@ref) `M`=$\operatorname{St}(n,k,B)$,
-i.e. `(n,k)`, which is the matrix dimensions.
+@doc doc"""
+    check_tangent_vector(M::GeneralizedStiefel, x, v; kwargs...)
+
+Check whether `v` is a valid tangent vector at `x` on the [`Stiefel`](@ref)
+`M`=$\operatorname{St}(n,k,B)$, i.e. the [`AbstractNumbers`](@ref) fits,
+`x` is a valid point on `M` and
+it (approximately) holds that $x^{\mathrm{H}}Bv + v^{\mathrm{H}}Bx = 0$, where
+`kwargs...` is passed to the `isapprox`.
 """
-@generated representation_size(::GeneralizedStiefel{n,k}) where {n,k} = (n, k)
+function check_tangent_vector(M::GeneralizedStiefel{n,k,T}, x, v; kwargs...) where {n,k,T}
+    mpe = check_manifold_point(M, x, kwargs)
+    mpe === nothing || return mpe
+    if (T === ℝ) && !(eltype(v) <: Real)
+        return DomainError(
+            eltype(v),
+            "The matrix $(v) is not a real-valued matrix, so it can not be a tangent vector to the Generalized Stiefel manifold of dimension ($(n),$(k)).",
+        )
+    end
+    if (T === ℂ) && !(eltype(v) <: Real) && !(eltype(v) <: Complex)
+        return DomainError(
+            eltype(v),
+            "The matrix $(v) is a neither real- nor complex-valued matrix, so it can not be a tangent vector to the complex Generalized Stiefel manifold of dimension ($(n),$(k)).",
+        )
+    end
+    if any(size(v) != representation_size(M))
+        return DomainError(
+            size(v),
+            "The matrix $(v) does not lie in the tangent space of $(x) on the Generalized Stiefel manifold of dimension ($(n),$(k)), since its dimensions are wrong.",
+        )
+    end
+    if !isapprox(x' * M.B * v + v' * M.B * x, zeros(k, k); kwargs...)
+        return DomainError(
+            norm(x' * v + v' * x),
+            "The matrix $(v) does not lie in the tangent space of $(x) on the Generalized Stiefel manifold of dimension ($(n),$(k)), since x'Bv + v'Bx is not the zero matrix.",
+        )
+    end
+end
