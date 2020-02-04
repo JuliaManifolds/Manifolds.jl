78a9b742842bef6f6b22e89afcc5eb702cf41ae7
Ronny Bergmann <git@ronnybergmann.net>
2020-02-04 10:09:33 +0100

    Update src/manifolds/ProductManifold.jl
    Co-Authored-By: Seth Axen <seth.axen@gmail.com>

--- src/manifolds/ProductManifold.jl
+++ src/manifolds/ProductManifold.jl
@@ -206,7 +206,7 @@ end
 @doc raw"""
     exp(M::ProductManifold, p, X)
 
-compute the exponential map from `p` towards `X` on the [`ProductManifold`](@ref) `M`,
+compute the exponential map from `p` in the direction of `X` on the [`ProductManifold`](@ref) `M`,
 which is the elementwise exponential map on the internal manifolds that build `M`.
 """
 exp(::ProductManifold, ::Any...)
