ac14d6313dca0de0575c8d3e2c47d38bc8ec76c8
Ronny Bergmann <git@ronnybergmann.net>
2020-03-10 20:04:11 +0100

    fix semi direct group.

--- test/groups/product_group.jl
+++ test/groups/product_group.jl
@@ -15,19 +15,21 @@ include("group_utils.jl")
     @test sprint(show, G) == "ProductGroup($(SOn), $(Tn))"
     @test sprint(show, "text/plain", G) == "ProductGroup with 2 subgroups:\n $(SOn)\n $(Tn)"
     x = Matrix{Float64}(I, 3, 3)
-
     t = Vector{Float64}.([1:2, 2:3, 3:4])
     ω = [[1.0, 2.0, 3.0], [3.0, 2.0, 1.0], [1.0, 3.0, 2.0]]
     tuple_pts = [(exp(Rn, x, hat(Rn, x, ωi)), ti) for (ωi, ti) in zip(ω, t)]
     tuple_v = (hat(Rn, x, [1.0, 0.5, -0.5]), [-1.0, 2.0])
+    eA = [x, zeros(2)]
+    shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
+    e = Manifolds.prod_point(shape_se, eA...)
 
     @testset "identity specializations" begin
-        @test inv(G, Identity(G)) === Identity(G)
-        @test identity(G, Identity(G)) === Identity(G)
-        @test submanifold_component(G, Identity(G), Val(1)) === Identity(SOn)
-        @test submanifold_component(G, Identity(G), Val(2)) === Identity(Tn)
-        @test submanifold_components(G, Identity(G)) == (Identity(SOn), Identity(Tn))
-        @test compose(G, Identity(G), Identity(G)) === Identity(G)
+        @test inv(G, Identity(G,e)) === Identity(G,e)
+        @test identity(G, Identity(G,e)) === Identity(G,e)
+        @test submanifold_component(G, Identity(G,e), Val(1)) == Identity(SOn,x)
+        @test submanifold_component(G, Identity(G,e), Val(2)) == Identity(Tn, zeros(2))
+        @test submanifold_components(G, Identity(G,e)) == (Identity(SOn,x), Identity(Tn,zeros(2)))
+        @test compose(G, Identity(G,e), Identity(G,e)) === Identity(G,e)
     end
 
     @testset "product point" begin
@@ -36,8 +38,8 @@ include("group_utils.jl")
             shape_se = Manifolds.ShapeSpecification(reshaper, M.manifolds...)
             pts = [Manifolds.prod_point(shape_se, tp...) for tp in tuple_pts]
             v_pts = [Manifolds.prod_point(shape_se, tuple_v...)]
-            @test compose(G, pts[1], Identity(G)) == pts[1]
-            @test compose(G, Identity(G), pts[1]) == pts[1]
+            @test compose(G, pts[1], Identity(G,e)) == pts[1]
+            @test compose(G, Identity(G,e), pts[1]) == pts[1]
             test_group(G, pts, v_pts, v_pts; test_diff = true)
             @test isapprox(
                 M,
@@ -65,8 +67,8 @@ include("group_utils.jl")
     @testset "product repr" begin
         pts = [ProductRepr(tp...) for tp in tuple_pts]
         v_pts = [ProductRepr(tuple_v...)]
-        @test compose(G, pts[1], Identity(G)) == pts[1]
-        @test compose(G, Identity(G), pts[1]) == pts[1]
+        @test compose(G, pts[1], Identity(G,e)) == pts[1]
+        @test compose(G, Identity(G,e), pts[1]) == pts[1]
         test_group(G, pts, v_pts, v_pts; test_diff = true, test_mutating = false)
         @test isapprox(
             M,
