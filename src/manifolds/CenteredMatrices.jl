e64feb5d91f413efe1b0df0fccbdfd5425159341
Renee Dornig <renee.dornig@s2015.tu-chemnitz.de>
2020-04-27 18:10:12 +0200

    add tolerance to check_manifold_point, define M as an AbstractEmbeddedManifold, modify documentation, add functions check_tangent_vector, embed, manifold_dimension, project and Base.show

--- src/manifolds/CenteredMatrices.jl
+++ src/manifolds/CenteredMatrices.jl
@@ -1,32 +1,137 @@
 @doc raw"""
-    CenteredMatrices{m,n,𝔽} <: Manifold{𝔽}
+    CenteredMatrices{m,n,𝔽} <: AbstractEmbeddedManifold{𝔽,TransparentIsometricEmbedding}
 
-The manifold of $m × n$ real-valued or complex-valued matrices whose columns sum to zero.
+The manifold of $m × n$ real-valued or complex-valued matrices whose columns sum to zero, i.e.
+````math
+\bigl\{ p ∈ 𝔽^{m × n}\ \big|\ [1 … 1] p = [0 … 0] \bigr\},
+````
+where $𝔽 ∈ \{ℝ,ℂ\}$.
 
 # Constructor
     CenteredMatrices(m, n[, field=ℝ])
 
 Generate the manifold of `m`-by-`n` (`field`-valued) matrices whose columns sum to zero.
 """
-struct CenteredMatrices{M,N,𝔽} <: Manifold{𝔽} end
+struct CenteredMatrices{M,N,𝔽} <: AbstractEmbeddedManifold{𝔽,TransparentIsometricEmbedding} end
+
 function CenteredMatrices(m::Int, n::Int, field::AbstractNumbers = ℝ)
     return CenteredMatrices{m,n,field}()
 end
 
 @doc raw"""
-    check_manifold_point(M::CenteredMatrices{m,n,𝔽}, p) 
+    check_manifold_point(M::CenteredMatrices{m,n,𝔽}, p; kwargs...) 
 
 Check whether the matrix is a valid point on the
 [`CenteredMatrices`](@ref) `M`, i.e. is an `m`-by-`n` matrix whose columns sum to 
 zero.
+
+The tolerance for the column sums of `p` can be set using `kwargs...`.
 """
-function check_manifold_point(M::CenteredMatrices{m,n}, p) where {m,n}
-    s = "The point $(p) does not lie on $(M), "
-    if size(p) != (m, n)
-        return DomainError(size(p), string(s, "since its size is wrong."))
-    end
-    if sum(p, dims=1) != zeros(1,n)
-        return DomainError(p, string(s, "since its columns do not sum to zero."))
+function check_manifold_point(M::CenteredMatrices{m,n}, p; kwargs...) where {m,n}
+    mpv =
+        invoke(check_manifold_point, Tuple{supertype(typeof(M)),typeof(p)}, M, p; kwargs...)
+    mpv === nothing || return mpv
+    if !isapprox(sum(p, dims=1), zeros(1,n); kwargs...)
+        return DomainError(p, string("The point $(p) does not lie on $(M), since its columns do not sum to zero."))
     end
     return nothing
+end
+
+
+"""
+    check_tangent_vector(M::CenteredMatrices{m,n,𝔽}, p, X; check_base_point = true, kwargs... )
+
+Check whether `X` is a tangent vector to manifold point `p` on the
+[`CenteredMatrices`](@ref) `M`, i.e. that `X` is a matrix of size `(m,n)` whose columns
+sum to zero and its values are from the correct [`AbstractNumbers`](@ref).
+The optional parameter `check_base_point` indicates, whether to call
+ [`check_manifold_point`](@ref)  for `p`.
+The tolerance for the column sums of `p` and `X` can be set using `kwargs...`.
+"""
+function check_tangent_vector(
+    M::CenteredMatrices{m,n,𝔽},
+    p,
+    X;
+    check_base_point = true,
+    kwargs...,
+) where {m,n,𝔽}
+    if check_base_point
+        mpe = check_manifold_point(M, p; kwargs...)
+        mpe === nothing || return mpe
+    end
+    mpv = invoke(
+        check_tangent_vector,
+        Tuple{supertype(typeof(M)),typeof(p),typeof(X)},
+        M,
+        p,
+        X;
+        check_base_point = false, 
+        kwargs...,
+    )
+    mpv === nothing || return mpv
+    if !isapprox(sum(X, dims=1), zeros(1,n); kwargs...)
+        return DomainError(
+            X,
+            "The vector $(X) is not a tangent vector to $(p) on $(M), since its columns do not sum to zero.",
+        )
+    end
+    return nothing
+end
+
+decorated_manifold(M::CenteredMatrices{m,n,𝔽}) where {m,n,𝔽} = Euclidean(m, n; field = 𝔽)
+
+embed!(M::CenteredMatrices, q, p) = copyto!(q, p)
+embed!(M::CenteredMatrices, Y, p, X) = copyto!(Y, X)
+
+@doc raw"""
+    manifold_dimension(M::CenteredMatrices{m,n,𝔽})
+
+Return the manifold dimension of the [`CenteredMatrices`](@ref) `m`-by-`n` matrix `M` over the number system
+`𝔽`, i.e.
+
+````math
+\dim(\mathcal M) = (m*n - m) \dim_ℝ 𝔽,
+````
+where $\dim_ℝ 𝔽$ is the [`real_dimension`](@ref) of `𝔽`.
+"""
+function manifold_dimension(::CenteredMatrices{m,n,𝔽}) where {m,n,𝔽}
+    return (m*n - m) * real_dimension(𝔽) 
+end
+
+@doc raw"""
+    project(M::CenteredMatrices, p)
+
+Projects `p` from the embedding onto the [`CenteredMatrices`](@ref) `M`, i.e.
+
+````math
+\operatorname{proj}_{\mathcal M}(p) = p - \begin{pmatrix} 
+\frac{1}{m}\sum_{i=1}^m p_{i,1} & ⋯ & \frac{1}{m}\sum_{i=1}^m p_{i,n}\\
+⋮ & & ⋮ \\
+\frac{1}{m}\sum_{i=1}^m p_{i,1} & ⋯ & \frac{1}{m}\sum_{i=1}^m p_{i,n}
+\end{pmatrix}.
+````
+"""
+project(::CenteredMatrices, ::Any)
+
+project!(M::CenteredMatrices, q, p) = copyto!(q, p .- mean(p,dims=1))
+
+@doc raw"""
+    project(M::CenteredMatrices, p, X)
+
+Project the matrix `X` onto the tangent space at `p` on the [`CenteredMatrices`](@ref) `M`, i.e.
+
+````math
+\operatorname{proj}_p(X) = X - \begin{pmatrix} 
+\frac{1}{m}\sum_{i=1}^m x_{i,1} & ⋯ & \frac{1}{m}\sum_{i=1}^m x_{i,n}\\
+⋮ & & ⋮ \\
+\frac{1}{m}\sum_{i=1}^m x_{i,1} & ⋯ & \frac{1}{m}\sum_{i=1}^m x_{i,n}
+\end{pmatrix}.
+````
+"""
+project(::CenteredMatrices, ::Any, ::Any)
+
+project!(M::CenteredMatrices, Y, p, X) = (Y .= X .- mean(X,dims=1))
+
+function Base.show(io::IO, ::CenteredMatrices{m,n,F}) where {m,n,F}
+    return print(io, "CenteredMatrices($(m), $(n), $(F))")
 end
