291a8a308f09417d691af781600215a7af4e1dcb
Mateusz Baran <mateuszbaran89@gmail.com>
2020-04-29 19:44:34 +0200

    formatting improvements

--- test/power_manifold.jl
+++ test/power_manifold.jl
@@ -121,8 +121,8 @@ Random.seed!(42)
         p2 = [10.0, 11.0, 12.0]
         set_component!(Ms1, p1, p2, 2)
         @test get_component(Ms1, p1, 2) == p2
-        p1[Ms1, 2] = 2*p2
-        @test p1[Ms1, 2] == 2*p2
+        p1[Ms1, 2] = 2 * p2
+        @test p1[Ms1, 2] == 2 * p2
 
         Msn1 = PowerManifold(Ms, Manifolds.NestedPowerRepresentation(), 5)
         pn1 = [randn(3) for _ in 1:5]
@@ -130,8 +130,8 @@ Random.seed!(42)
         @test pn1[Msn1, 1] == pn1[1]
         set_component!(Msn1, pn1, p2, 2)
         @test get_component(Msn1, pn1, 2) == p2
-        pn1[Msn1, 2] = 2*p2
-        @test pn1[Msn1, 2] == 2*p2
+        pn1[Msn1, 2] = 2 * p2
+        @test pn1[Msn1, 2] == 2 * p2
     end
 
     @testset "power vector transport" begin
--- test/product_manifold.jl
+++ test/product_manifold.jl
@@ -45,13 +45,13 @@ struct NotImplementedReshaper <: Manifolds.AbstractReshaper end
         p2 = [10.0, 12.0]
         set_component!(Mse, p1, p2, 2)
         @test get_component(Mse, p1, 2) == p2
-        p1[Mse, 2] = 2*p2
-        @test p1[Mse, 2] == 2*p2
+        p1[Mse, 2] = 2 * p2
+        @test p1[Mse, 2] == 2 * p2
         p3 = [11.0, 15.0]
         set_component!(Mse, p1, p3, Val(2))
         @test get_component(Mse, p1, Val(2)) == p3
-        p1[Mse, Val(2)] = 2*p3
-        @test p1[Mse, Val(2)] == 2*p3
+        p1[Mse, Val(2)] = 2 * p3
+        @test p1[Mse, Val(2)] == 2 * p3
 
         shape_a_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M1, M2)
         pra1 = Manifolds.ProductArray(shape_a_se, [0.0, 1.0, 0.0, 0.0, 0.0])
@@ -61,12 +61,12 @@ struct NotImplementedReshaper <: Manifolds.AbstractReshaper end
         @test pra1[Mse, Val(1)] == pra1.parts[1]
         set_component!(Mse, pra1, p2, 2)
         @test get_component(Mse, pra1, 2) == p2
-        pra1[Mse, 2] = 2*p2
-        @test pra1[Mse, 2] == 2*p2
+        pra1[Mse, 2] = 2 * p2
+        @test pra1[Mse, 2] == 2 * p2
         set_component!(Mse, pra1, p3, Val(2))
         @test get_component(Mse, pra1, Val(2)) == p3
-        pra1[Mse, Val(2)] = 2*p3
-        @test pra1[Mse, Val(2)] == 2*p3
+        pra1[Mse, Val(2)] = 2 * p3
+        @test pra1[Mse, Val(2)] == 2 * p3
 
         shape_s_se = Manifolds.ShapeSpecification(Manifolds.StaticReshaper(), M1, M2)
         prs1 = Manifolds.ProductArray(shape_s_se, [0.0, 1.0, 0.0, 0.0, 0.0])
@@ -76,12 +76,12 @@ struct NotImplementedReshaper <: Manifolds.AbstractReshaper end
         @test prs1[Mse, Val(1)] == prs1.parts[1]
         set_component!(Mse, prs1, p2, 2)
         @test get_component(Mse, prs1, 2) == p2
-        prs1[Mse, 2] = 2*p2
-        @test prs1[Mse, 2] == 2*p2
+        prs1[Mse, 2] = 2 * p2
+        @test prs1[Mse, 2] == 2 * p2
         set_component!(Mse, prs1, p3, Val(2))
         @test get_component(Mse, prs1, Val(2)) == p3
-        prs1[Mse, Val(2)] = 2*p3
-        @test prs1[Mse, Val(2)] == 2*p3
+        prs1[Mse, Val(2)] = 2 * p3
+        @test prs1[Mse, Val(2)] == 2 * p3
     end
 
     @testset "Show methods" begin
