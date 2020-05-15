de08749aaa081d683bfc1b055ad0d5e5bb5109bb
Mateusz Baran <mateuszbaran89@gmail.com>
2020-05-15 16:25:47 +0200

    Please checkout the correct commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,8 +20,7 @@ end
 
 @testset "Ambiguities" begin
     # TODO: reduce the number of ambiguities
-    base_ambigs = Test.detect_ambiguities(ManifoldsBase)
-    @test length(Test.detect_ambiguities(ManifoldsBase)) <= 13
+    @test length(Test.detect_ambiguities(ManifoldsBase)) <= 12
     @test length(Test.detect_ambiguities(Manifolds)) == 0
     @test length(our_base_ambiguities()) <= 21
 end
