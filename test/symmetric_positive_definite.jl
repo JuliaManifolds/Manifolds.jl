aaac8ded44531cbc9879ea2cd43a07344bb14a12
Mateusz Baran <mateuszbaran89@gmail.com>
2020-05-16 10:42:43 +0200

    formatting fixed

--- test/symmetric_positive_definite.jl
+++ test/symmetric_positive_definite.jl
@@ -62,9 +62,7 @@ using Manifolds: default_metric_dispatch
                     M,
                     pts;
                     test_vector_transport = true,
-                    vector_transport_methods = [
-                        ParallelTransport(),
-                    ],
+                    vector_transport_methods = [ParallelTransport()],
                     test_forward_diff = false,
                     test_reverse_diff = false,
                     test_vee_hat = M === M2,
@@ -124,21 +122,21 @@ using Manifolds: default_metric_dispatch
         p1 = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1]
         p2 = [2.0 0.0 0.0; 0.0 2.0 0.0; 0.0 0.0 1]
         p3 = A(π / 6) * [1.0 0.0 0.0; 0.0 2.0 0.0; 0.0 0.0 1] * transpose(A(π / 6))
-        X1 = log(M,p1, p3)
+        X1 = log(M, p1, p3)
         Y1 = vector_transport_to(M, p1, X1, p2)
-        @test is_tangent_vector(M,p2,Y1)
+        @test is_tangent_vector(M, p2, Y1)
         Y2 = vector_transport_to(M, p1, X1, p2, PoleLadderTransport())
-        @test is_tangent_vector(M,p2,Y2,atol=10^-16)
+        @test is_tangent_vector(M, p2, Y2, atol = 10^-16)
         Y3 = vector_transport_to(M, p1, X1, p2, SchildsLadderTransport())
-        @test is_tangent_vector(M,p2,Y3)
-        @test isapprox(M,p2,Y1,Y2) # pole is exact on SPDs, i.e. identical to parallel transport
-        @test norm(M,p1,X1) ≈ norm(M,p2,Y1) # parallel transport is length preserving
+        @test is_tangent_vector(M, p2, Y3)
+        @test isapprox(M, p2, Y1, Y2) # pole is exact on SPDs, i.e. identical to parallel transport
+        @test norm(M, p1, X1) ≈ norm(M, p2, Y1) # parallel transport is length preserving
         # test isometry
         X2 = log(M, p1, p2)
         Y4 = vector_transport_to(M, p1, X2, p2)
-        @test is_tangent_vector(M,p2,Y4)
+        @test is_tangent_vector(M, p2, Y4)
         Y5 = vector_transport_to(M, p1, X2, p2, PoleLadderTransport())
-        @test inner(M,p1,X1,X2) ≈ inner(M,p2,Y1,Y4) # parallel transport isometric
-        @test inner(M,p1,X1,X2) ≈ inner(M,p2,Y2,Y5) # pole ladder transport isometric
+        @test inner(M, p1, X1, X2) ≈ inner(M, p2, Y1, Y4) # parallel transport isometric
+        @test inner(M, p1, X1, X2) ≈ inner(M, p2, Y2, Y5) # pole ladder transport isometric
     end
 end
