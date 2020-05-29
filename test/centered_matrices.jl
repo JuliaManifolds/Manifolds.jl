e4690c450ad27eda146d4ed164cb96388304d5b1
Renée Dornig <renee.dornig@s2015.tu-chemnitz.de>
2020-05-29 09:35:44 +0200

    adds test for complex centered matrices

--- test/centered_matrices.jl
+++ test/centered_matrices.jl
@@ -3,12 +3,11 @@ include("utils.jl")
 @testset "CenteredMatrices" begin
     M = CenteredMatrices(3, 2)
     M_complex = CenteredMatrices(3, 2, ℂ)
-    @test repr(M_complex) == "CenteredMatrices(3, 2, ℂ)"
-    @test manifold_dimension(M_complex) == 8
     A = [1 2; 4 5; -5 -7]
     B = [1 2 3; 4 5 6; -5 -7 -9]    #wrong dimensions
     C = [-3 -im; 2 im; 1 0]         #complex
     D = [1 2; 3 4; 5 6]             #not centered 
+    types = [Matrix{Float64}]
     @testset "Real Centered Matrices Basics" begin
         @test repr(M) == "CenteredMatrices(3, 2, ℝ)"
         @test representation_size(M) == (3, 2)
@@ -30,32 +29,35 @@ include("utils.jl")
         A3 = embed(M, A)
         @test A2 == A
         @test A3 == A
+        E = [0 0; 1 -1; -1 1]
+        F = [0.5 1; -1 -0.7; 0.5 -0.3]
+        test_manifold(
+            M,
+            [A, E, F],
+            test_injectivity_radius = false,
+            test_reverse_diff = false,
+            test_project_tangent = true,
+            test_musical_isomorphisms = true,
+            test_vector_transport = true,
+            is_tangent_atol_multiplier = 1,
+            is_point_atol_multiplier = 1,
+        )
     end
-    types = [Matrix{Float64}]
-    E = [0 0; 1 -1; -1 1]
-    F = [0.5 1; -1 -0.7; 0.5 -0.3]
-    test_manifold(
-        M,
-        [A, E, F],
-        test_injectivity_radius = false,
-        test_reverse_diff = false,
-        test_project_tangent = true,
-        test_musical_isomorphisms = true,
-        test_vector_transport = true,
-        is_tangent_atol_multiplier = 1,
-        is_point_atol_multiplier = 1,
-    )
-    G = [1 im; -im 0; -1+im -im]
-    H = [im 0; -2*im im; im -im]
-    test_manifold(
-        M_complex,
-        [C, G, H],
-        test_injectivity_radius = false,
-        test_reverse_diff = false,
-        test_project_tangent = true,
-        test_musical_isomorphisms = true,
-        test_vector_transport = true,
-        is_tangent_atol_multiplier = 1,
-        is_point_atol_multiplier = 1,
-    )
+    @testset "Complex Centered Matrices Basics" begin
+        @test repr(M_complex) == "CenteredMatrices(3, 2, ℂ)"
+        @test manifold_dimension(M_complex) == 8
+        G = [1.0 1.0im; -1.0im 0.0; -1.0+1.0im -1.0im]
+        H = [1.0im 0.0; -2.0im 1.0im; 1.0im -1.0im]
+        test_manifold(
+            M_complex,
+            [C, G, H],
+            test_injectivity_radius = false,
+            test_reverse_diff = false,
+            test_project_tangent = true,
+            test_musical_isomorphisms = true,
+            test_vector_transport = true,
+            is_tangent_atol_multiplier = 1,
+            is_point_atol_multiplier = 1,
+        )
+    end
 end
