925048bce54d9f1c94ddb163677313490bc37b68
Renée Dornig <renee.dornig@s2015.tu-chemnitz.de>
2020-06-02 15:07:48 +0200

    sets is_tangent_atol_multiplier in test_manifold in test/sphere_symmetric_matrices.jl to 2

--- test/sphere_symmetric_matrices.jl
+++ test/sphere_symmetric_matrices.jl
@@ -44,7 +44,7 @@ include("utils.jl")
         test_project_tangent = true,
         test_musical_isomorphisms = true,
         test_vector_transport = true,
-        is_tangent_atol_multiplier = 1,
+        is_tangent_atol_multiplier = 2,
         #is_point_atol_multilplier = 1,
     )
 end
