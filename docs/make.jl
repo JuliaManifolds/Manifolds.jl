49413ed278e20d2794d7c6125aa4f5d1eb091477
Ronny Bergmann <git@ronnybergmann.net>
2020-09-27 14:08:01 +0200

    Introduces a approach solely based on github actions.

--- docs/make.jl
+++ docs/make.jl
@@ -80,22 +80,4 @@ makedocs(
         ],
     ],
 )
-
-function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
-    event_path === nothing && return false
-    event = JSON.parsefile(event_path)
-    labels = [x["name"] for x in event["pull_request"]["labels"]]
-    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
-    yes = "deploydocs" in labels
-    if yes
-        @info "Trying to push preview as label `deploydocs` is specified." labels
-    else
-        @info "Not pushing preview as label `deploydocs` is not specified." labels
-    end
-    return yes
-end
-
-deploydocs(
-    repo = "github.com/JuliaManifolds/Manifolds.jl.git",
-    push_preview = should_push_preview(),
-)
+deploydocs(repo = "github.com/JuliaManifolds/Manifolds.jl.git", push_preview = true)
