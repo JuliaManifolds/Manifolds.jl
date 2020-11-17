96f486989c9349f5c9873db92b747e244c3305d9
Ronny Bergmann <git@ronnybergmann.net>
2020-11-17 11:01:49 +0100

    remove an unnecessary case for the tangent bundle.

--- src/manifolds/VectorBundle.jl
+++ src/manifolds/VectorBundle.jl
@@ -633,14 +633,10 @@ function inner(B::VectorBundle, p, X, Y)
     px, Vx = submanifold_components(B.manifold, p)
     VXM, VXF = submanifold_components(B.manifold, X)
     VYM, VYF = submanifold_components(B.manifold, Y)
-    return inner(B.manifold, px, VXM, VYM) + inner(VectorSpaceAtPoint(B.fiber, px), Vx, VXF, VYF)
+    return inner(B.manifold, px, VXM, VYM) +
+           inner(VectorSpaceAtPoint(B.fiber, px), Vx, VXF, VYF)
 end
-function inner(B::TangentBundle, p, X, Y)
-    px, Vx = submanifold_components(B.manifold, p)
-    VXM, VXF = submanifold_components(B.manifold, X)
-    VYM, VYF = submanifold_components(B.manifold, Y)
-    return inner(B.manifold, px, VXM, VYM) + inner(B.manifold, px, VXF, VYF)
-end
+
 """
     inner(M::TangentSpaceAtPoint, p, X, Y)
 
