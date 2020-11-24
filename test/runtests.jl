bf8bdf16bac5a92c157f2d7d6a7adfbf8520ce23
Ronny Bergmann <git@ronnybergmann.net>
2020-11-24 23:25:05 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ end
 (VERSION >= v"1.1") && @testset "Ambiguities" begin
     # TODO: reduce the number of ambiguities
     if VERSION >= v"1.6-DEV"
-        @test length(Test.detect_ambiguities(ManifoldsBase)) <= 3
+        @test length(Test.detect_ambiguities(ManifoldsBase)) <= 4
         @test length(Test.detect_ambiguities(Manifolds)) <= 102
         @test length(our_base_ambiguities()) <= 4
     else
