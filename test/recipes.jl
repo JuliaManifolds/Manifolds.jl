3c38b42246c528157d779a67432489349b7df932
Ronny Bergmann <git@ronnybergmann.net>
2020-11-24 20:32:06 +0100

    runs formatter.

--- test/recipes.jl
+++ test/recipes.jl
@@ -7,35 +7,35 @@ include("utils.jl")
 @testset "Recipes Test" begin
     function Hyp2PB_plot()
         M = Hyperbolic(2)
-        p = Manifolds._hyperbolize.(Ref(M), [[1.0,0.0], [0.0,1.0]])
-        p2 = convert.( Ref(PoincareBallPoint), p)
-        plot(M,p2)
+        p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
+        p2 = convert.(Ref(PoincareBallPoint), p)
+        return plot(M, p2)
     end
     @plottest Hyp2PB_plot "assets/Hyp2PBPlot.png" false
 
     function Hyp2PB_quiver()
         M = Hyperbolic(2)
-        p = Manifolds._hyperbolize.(Ref(M), [[1.0,0.0], [0.0,0.0], [0.0,1.0]])
-        p2 = convert.( Ref(PoincareBallPoint), p)
-        X = [ log(M,p2[2],p2[1]), log(M,p2[1],p2[3]) ]
-        plot(M,[p2[2],p2[1]], X)
+        p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
+        p2 = convert.(Ref(PoincareBallPoint), p)
+        X = [log(M, p2[2], p2[1]), log(M, p2[1], p2[3])]
+        return plot(M, [p2[2], p2[1]], X)
     end
     @plottest Hyp2PB_quiver "assets/Hyp2PBQuiver.png" false
 
     function Hyp2PH_plot()
         M = Hyperbolic(2)
-        p = Manifolds._hyperbolize.(Ref(M), [[1.0,0.0], [0.0,1.0]])
-        p2 = convert.( Ref(PoincareHalfSpacePoint), p)
-        plot(M,p2)
+        p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
+        p2 = convert.(Ref(PoincareHalfSpacePoint), p)
+        return plot(M, p2)
     end
     @plottest Hyp2PH_plot "assets/Hyp2PHPlot.png" false
 
     function Hyp2PH_quiver()
         M = Hyperbolic(2)
-        p = Manifolds._hyperbolize.(Ref(M), [[1.0,0.0], [0.0,0.0], [0.0,1.0]])
-        p2 = convert.( Ref(PoincareHalfSpacePoint), p)
-        X = [ log(M,p2[2],p2[1]), log(M,p2[1],p2[3]) ]
-        plot(M,[p2[2],p2[1]], X)
+        p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
+        p2 = convert.(Ref(PoincareHalfSpacePoint), p)
+        X = [log(M, p2[2], p2[1]), log(M, p2[1], p2[3])]
+        return plot(M, [p2[2], p2[1]], X)
     end
     @plottest Hyp2PH_quiver "assets/Hyp2PHQuiver.png" false
 end
