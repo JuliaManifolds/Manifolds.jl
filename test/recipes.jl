bc59bcf1776b455e478e21fca499cf936854ae92
Ronny <git@ronnybergmann.net>
2020-12-10 19:35:47 +0100

    let's try another of all these cases that locally do work.

--- test/recipes.jl
+++ test/recipes.jl
@@ -1,4 +1,4 @@
-using RecipesBase, VisualRegressionTests, Plots, Colors
+using RecipesBase, VisualRegressionTests, Plots, Colors, PyPlot
 include("utils.jl")
 # Note that the `false`s avoid popups and the tests directly fail.
 # If you have changed something and need to recreate the reference in the test avoids to start Gtk
@@ -12,7 +12,7 @@ include("utils.jl")
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareBallPoint), p)
-            return plot(M, p2)
+            return Plots.plot(M, p2)
         end
         @plottest Hyp2PB_plot joinpath(references_folder, "Hyp2PBPlot.png") false
 
@@ -20,7 +20,7 @@ include("utils.jl")
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareBallPoint), p)
-            return plot(M, p2; geodesic_interpolation = 80)
+            return Plots.plot(M, p2; geodesic_interpolation = 80)
         end
         @plottest Hyp2PB_plot_geo joinpath(references_folder, "Hyp2PBPlotGeo.png") false
 
@@ -29,7 +29,7 @@ include("utils.jl")
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareBallPoint), p)
             X = [log(M, p2[2], p2[1]), log(M, p2[1], p2[3])]
-            return plot(M, [p2[2], p2[1]], X)
+            return Plots.plot(M, [p2[2], p2[1]], X)
         end
         @plottest Hyp2PB_quiver joinpath(references_folder, "Hyp2PBQuiver.png") false
 
@@ -37,7 +37,7 @@ include("utils.jl")
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareHalfSpacePoint), p)
-            return plot(M, p2)
+            return Plots.plot(M, p2)
         end
         @plottest Hyp2PH_plot joinpath(references_folder, "Hyp2PHPlot.png") false
 
@@ -45,7 +45,7 @@ include("utils.jl")
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareHalfSpacePoint), p)
-            return plot(M, p2; geodesic_interpolation = 80)
+            return Plots.plot(M, p2; geodesic_interpolation = 80)
         end
         @plottest Hyp2PH_plot_geo joinpath(references_folder, "Hyp2PHPlotGeo.png") false
 
@@ -54,7 +54,7 @@ include("utils.jl")
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
             p2 = convert.(Ref(PoincareHalfSpacePoint), p)
             X = [log(M, p2[2], p2[1]), log(M, p2[1], p2[3])]
-            return plot(M, [p2[2], p2[1]], X)
+            return Plots.plot(M, [p2[2], p2[1]], X)
         end
         @plottest Hyp2PH_quiver joinpath(references_folder, "Hyp2PHQuiver.png") false
     end
@@ -64,14 +64,14 @@ include("utils.jl")
         function Hyp2_plot()
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
-            return plot(M, p)
+            return Plots.plot(M, p)
         end
         @plottest Hyp2_plot joinpath(references_folder, "Hyp2Plot.png") true
 
         function Hyp2_plot_geo()
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 1.0]])
-            return plot(M, p; geodesic_interpolation = 80)
+            return Plots.plot(M, p; geodesic_interpolation = 80)
         end
         @plottest Hyp2_plot_geo joinpath(references_folder, "Hyp2PlotGeo.png") false
 
@@ -79,7 +79,7 @@ include("utils.jl")
             M = Hyperbolic(2)
             p = Manifolds._hyperbolize.(Ref(M), [[1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
             X = [log(M, p[2], p[1]), log(M, p[1], p[3])]
-            return plot(M, [p[2], p[1]], X)
+            return Plots.plot(M, [p[2], p[1]], X)
         end
         @plottest Hyp2_quiver joinpath(references_folder, "Hyp2Quiver.png") false
     end
@@ -88,14 +88,14 @@ include("utils.jl")
         function Sphere2_plot()
             M = Sphere(2)
             pts = [[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]]
-            return plot(M, pts; wireframe_color = colorant"#CCCCCC", markersize = 10)
+            return Plots.plot(M, pts; wireframe_color = colorant"#CCCCCC", markersize = 10)
         end
         @plottest Sphere2_plot joinpath(references_folder, "Sphere2Plot.png") false
 
         function Sphere2_plot_geo()
             M = Sphere(2)
             pts = [[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]]
-            return plot(
+            return Plots.plot(
                 M,
                 pts;
                 wireframe_color = colorant"#CCCCCC",
@@ -110,7 +110,7 @@ include("utils.jl")
             pts2 = [[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, 1.0]]
             p3 = 1 / sqrt(3) .* [1.0, -1.0, 1.0]
             vecs = log.(Ref(M), pts2, Ref(p3))
-            return plot(M, pts2, vecs; wireframe_color = colorant"#CCCCCC", linewidth = 1.5)
+            return Plots.plot(M, pts2, vecs; wireframe_color = colorant"#CCCCCC", linewidth = 1.5)
         end
         @plottest Sphere2_quiver joinpath(references_folder, "Sphere2Quiver.png") false
     end
