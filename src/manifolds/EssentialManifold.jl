4d69ad4b0a005e255ff0850b74104c52c75d5053
Renée Dornig <renee.dornig@mathematik.tu-chemnitz.de>
2021-01-04 21:08:43 +0100

    runs Formatter

--- src/manifolds/EssentialManifold.jl
+++ src/manifolds/EssentialManifold.jl
@@ -26,13 +26,12 @@ Generate the manifold of essential matrices.
     > DOI: [10.1137/16M1091332](https://doi.org/10.1137/16M1091332),
     > PDF: [https://www.cis.upenn.edu/~kostas/mypub.dir/tron17siam.pdf](https://www.cis.upenn.edu/~kostas/mypub.dir/tron17siam.pdf).
 """
-struct EssentialManifold <:
-    AbstractPowerManifold{ℝ, Rotations{3}, NestedPowerRepresentation} 
+struct EssentialManifold <: AbstractPowerManifold{ℝ,Rotations{3},NestedPowerRepresentation}
     is_signed::Bool
     manifold::Rotations{3}
 end
 
-EssentialManifold(is_signed::Bool = true) = EssentialManifold(is_signed, Rotations(3))  
+EssentialManifold(is_signed::Bool=true) = EssentialManifold(is_signed, Rotations(3))
 
 @doc raw"""
     check_manifold_point(M::EssentialManifold, p; kwargs...) 
@@ -40,14 +39,18 @@ EssentialManifold(is_signed::Bool = true) = EssentialManifold(is_signed, Rotatio
 Check whether the matrix is a valid point on the [`EssentialManifold`](@ref) `M`, 
 i.e. a Tuple of two SO(3) matrices.
 """
-function check_manifold_point(M::EssentialManifold, p; kwargs...) 
-    if length(p)!=2
+function check_manifold_point(M::EssentialManifold, p; kwargs...)
+    if length(p) != 2
         return DomainError(
             length(p),
             "The point $(p) does not lie on $M, since it does not contain exactly two elements.",
         )
     end
-    return check_manifold_point(PowerManifold(M.manifold, NestedPowerRepresentation(), 2), p; kwargs...)
+    return check_manifold_point(
+        PowerManifold(M.manifold, NestedPowerRepresentation(), 2),
+        p;
+        kwargs...,
+    )
     return nothing
 end
 
@@ -58,18 +61,12 @@ Check whether `X` is a tangent vector to manifold point `p` on the [`EssentialMa
 i.e. `X` has to be a Tuple of two `3`-by-`3` skew-symmetric matrices. The optional parameter `check_base_point` 
 indicates, whether to call [`check_manifold_point`](@ref)  for `p`.
 """
-function check_tangent_vector(
-    M::EssentialManifold,
-    p,
-    X;
-    check_base_point = true,
-    kwargs...,
-)
+function check_tangent_vector(M::EssentialManifold, p, X; check_base_point=true, kwargs...)
     if check_base_point
         mpe = check_manifold_point(M, p; kwargs...)
         mpe === nothing || return mpe
     end
-    if length(X)!=2
+    if length(X) != 2
         return DomainError(
             length(X),
             "$(X) is not a tangent vector to the manifold $M, since it does not contain exactly two elements.",
@@ -79,7 +76,7 @@ function check_tangent_vector(
         PowerManifold(M.manifold, NestedPowerRepresentation(), 2),
         p,
         X;
-        check_base_point = check_base_point,
+        check_base_point=check_base_point,
         kwargs...,
     )
     return nothing
@@ -121,11 +118,10 @@ where $\tilde X$ is the horizontal lift of $X$[^TronDaniilidis2017].
 exp(::EssentialManifold, ::Any...)
 
 function exp!(M::EssentialManifold, q, p, X)
-    exp!.(Ref(M.manifold),q, p, X)
-    q .= X.*q
+    exp!.(Ref(M.manifold), q, p, X)
+    q .= X .* q
     return q
 end
-
 
 get_iterator(M::EssentialManifold) = Base.OneTo(2)
 
@@ -163,32 +159,32 @@ details see [^TronDaniilidis2017].
 function log!(M::EssentialManifold, X, p, q)
     # compute the closest representative of q
     t = 0
-    f_min = Inf 
+    f_min = Inf
     q2 = deepcopy(q)
 
     if !M.is_signed
-        for k = 1:4 
+        for k in 1:4
             #flip sign in q to get another member of its equivalence class
             if k == 2
                 q2[1][2:3, :] = -q[1][2:3, :]
-                q2[2][[1 3],:] = -q[2][[1 3],:]
+                q2[2][[1 3], :] = -q[2][[1 3], :]
             elseif k == 3
                 q2[2][1:2, :] = -q[2][1:2, :]
             elseif k == 4
                 q2[1][2:3, :] = -q[1][2:3, :]
                 q2[2][2:3, :] = -q[2][2:3, :]
-            end 
+            end
             t_temp, f_temp = dist_min_angle_pair(p, q2)
             if f_temp < f_min
-                f_min = f_temp 
+                f_min = f_temp
                 t = t_temp
             end
         end
     else
-        t, _ = dist_min_angle_pair(p, q2) 
+        t, _ = dist_min_angle_pair(p, q2)
     end
-        
-    Rz=[cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1]
+
+    Rz = [cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1]
     representative_q = zeros(size(q2))
     representative_q = [Rz * q2[1], Rz * q2[2]]
 
@@ -211,18 +207,18 @@ For more details see Algorithm 1 in [^TronDaniilidis2017].
 """
 function dist_min_angle_pair(p, q)
     #compute rotations
-    q211 = q[1]*p[1]'
-    q212 = q[2]*p[2]'
+    q211 = q[1] * p[1]'
+    q212 = q[2] * p[2]'
 
     #compute values from Proposition 9
     t_break1, c1, m1, Φ1 = dist_min_angle_pair_discontinuity_distance(q211)
     t_break2, c2, m2, Φ2 = dist_min_angle_pair_discontinuity_distance(q212)
-    
+
     #check if cost is constant
-    tolMZero = 1e-15 
+    tolMZero = 1e-15
     if (abs(m1) < tolMZero) && (abs(m2) < tolMZero)
         t_min = 0
-        f_min = 2*pi^2
+        f_min = 2 * pi^2
     else
         if abs(mod(t_break1 - t_break2 + pi, 2 * pi) - pi) < 1e-8
             t_min = t_break1 + pi
@@ -233,16 +229,16 @@ function dist_min_angle_pair(p, q)
             if t_search1 > t_search2
                 t_search1 = t_search1 - 2 * pi
             end
-            
+
             #compute derivatives of each term at discontinuity points
             df1_break1 = dist_min_angle_pair_compute_df_break(t_break1, q211)
             df2_break2 = dist_min_angle_pair_compute_df_break(t_break2, q212)
 
             #compute derivative of each term at other's discontinuity
-            θ1_break2 = acos(clamp((m1 * sin(t_break2 + Φ1) + c1 - 1) / 2 , -1.0, 1.0))
-            df1_break2= -θ1_break2 * (m1 * cos(t_break2 + Φ1)) / (2 * sin(θ1_break2))
-            θ2_break1 = acos(clamp((m2 * sin(t_break1 + Φ2) + c2 - 1) / 2 , -1.0, 1.0))
-            df2_break1= -θ2_break1 * (m2 * cos(t_break1 + Φ2)) / (2 * sin(θ2_break1))
+            θ1_break2 = acos(clamp((m1 * sin(t_break2 + Φ1) + c1 - 1) / 2, -1.0, 1.0))
+            df1_break2 = -θ1_break2 * (m1 * cos(t_break2 + Φ1)) / (2 * sin(θ1_break2))
+            θ2_break1 = acos(clamp((m2 * sin(t_break1 + Φ2) + c2 - 1) / 2, -1.0, 1.0))
+            df2_break1 = -θ2_break1 * (m2 * cos(t_break1 + Φ2)) / (2 * sin(θ2_break1))
 
             #compute left and right derivatives of sum of the two terms
             df_break1n = df1_break1 + df2_break1
@@ -253,23 +249,47 @@ function dist_min_angle_pair(p, q)
             f_min = Inf
             if sign(df_break1p) != sign(df_break2n)
                 #parabolic prediction of min
-                t_min0 = t_search1 - df_break1p * (t_search2 - t_search1) / (df_break2n - df_break1p)
+                t_min0 =
+                    t_search1 -
+                    df_break1p * (t_search2 - t_search1) / (df_break2n - df_break1p)
                 #use Newton's search to find t_min
-                t_min, f_min = dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min0, t_search1, t_search2)
+                t_min, f_min = dist_min_angle_pair_df_newton(
+                    m1,
+                    Φ1,
+                    c1,
+                    m2,
+                    Φ2,
+                    c2,
+                    t_min0,
+                    t_search1,
+                    t_search2,
+                )
             end
             t_search1 = t_search1 + 2 * pi
             if sign(df_break2p) != sign(df_break1n)
                 #parabolic prediction of min
-                t_min0 = t_search2 - df_break2p * (t_search1 - t_search2) / (df_break1n - df_break2p)
+                t_min0 =
+                    t_search2 -
+                    df_break2p * (t_search1 - t_search2) / (df_break1n - df_break2p)
                 #use Newton's search to find t_min
-                t_min2, f_min2 = dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min0, t_search2, t_search1)
+                t_min2, f_min2 = dist_min_angle_pair_df_newton(
+                    m1,
+                    Φ1,
+                    c1,
+                    m2,
+                    Φ2,
+                    c2,
+                    t_min0,
+                    t_search2,
+                    t_search1,
+                )
                 #choose t with lower function values
                 if f_min2 < f_min
                     t_min = t_min2
                     f_min = f_min2
                 end
             end
-        end    
+        end
     end
     return t_min, f_min
 end
@@ -290,7 +310,7 @@ function dist_min_angle_pair_discontinuity_distance(q)
     c3 = q[3, 3]
 
     m = norm([c1 c2])
-    Φ = atan(c1, c2) 
+    Φ = atan(c1, c2)
 
     t_break = -0.5 * pi - Φ
     return t_break, c3, m, Φ
@@ -301,12 +321,12 @@ end
 
 This function computes the derivatives of each term $f_i, i=1,2,$ at discontinuity point `t_break`. For more details see [^TronDaniilidis2017].
 """
-function dist_min_angle_pair_compute_df_break(t_break, q) 
+function dist_min_angle_pair_compute_df_break(t_break, q)
     c = cos(t_break)
     s = sin(t_break)
 
     q1_r_break_q1 = [c -s 0; s c 0; 0 0 1] * q
-    F = svd(q1_r_break_q1 + I(3))    
+    F = svd(q1_r_break_q1 + I(3))
     df_break = pi * abs(F.U[3, 1])
     return df_break
 end
@@ -322,13 +342,13 @@ in the interval $[$`t_low`, `t_high`$]$ using Newton's method. For more details 
 """
 function dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min, t_low, t_high)
     tol_dist = 1e-8
-    θ1 = 0  
-    θ2 = 0  
-    for i = 1:100
+    θ1 = 0
+    θ2 = 0
+    for i in 1:100
         #compute auxiliary values
         mc1 = m1 * cos(t_min + Φ1)
         mc2 = m2 * cos(t_min + Φ2)
-        
+
         #compute θi, i=1,2
         θ1 = acos(clamp(((m1 * sin(t_min + Φ1) + c1 - 1) / 2), -1, 1))
         θ2 = acos(clamp(((m2 * sin(t_min + Φ2) + c2 - 1) / 2), -1, 1))
@@ -336,10 +356,10 @@ function dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min, t_low, t
         #compute the first derivatives di, i=1,2
         s1 = 2 * sin(θ1)
         s2 = 2 * sin(θ2)
-        d1 = (-θ1 * mc1) / s1 
+        d1 = (-θ1 * mc1) / s1
         d2 = (-θ2 * mc2) / s2
         d = d1 + d2
-        
+
         #compute the second derivatives ddi, i=1,2
         eztuSq1 = (mc1 / s1)^2
         eztuSq2 = (mc2 / s2)^2
@@ -348,7 +368,7 @@ function dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min, t_low, t
         dd = dd1 + dd2
 
         #compute the new t_min
-        t_old = t_min;
+        t_old = t_min
         t_min = clamp(t_old - d / dd, t_low + tol_dist, t_high - tol_dist)
         if abs(t_min - t_old) < tol_dist
             break
@@ -357,7 +377,6 @@ function dist_min_angle_pair_df_newton(m1, Φ1, c1, m2, Φ2, c2, t_min, t_low, t
     f_min = θ1^2 + θ2^2
     return t_min, f_min
 end
-
 
 @doc raw"""
     manifold_dimension(M::EssentialManifold{is_signed, ℝ})
@@ -378,7 +397,7 @@ the space of all essential matrices is
 Since $SO(3)^2$ has dimension two, $H_z$ has dimension one and the discrete group $H_π$ does not change the dimension 
 of the space, the space of all essential matrices has dimension `5`[^TronDaniilidis2017].
 """
-function manifold_dimension(::EssentialManifold) 
+function manifold_dimension(::EssentialManifold)
     return 5
 end
 
@@ -399,14 +418,21 @@ and $R_0 ∈ \text{SO}(3)$ such that $R_0(T'_2-T'_1) = e_z$.
 """
 project(::EssentialManifold, ::Any, ::Any)
 
-function project!(M::EssentialManifold, Y, p, X) 
-    X_proj = p'.*X
+function project!(M::EssentialManifold, Y, p, X)
+    X_proj = p' .* X
     X_proj_skew = 0.5 * (X_proj - X_proj')
     # projection on the vertical component
     s = vert_proj(M, p, X_proj_skew)
     # orthogonal projection
-    copyto!(Y, X_proj_skew - (s/2) .* vcat(get_vector(M.manifold, p[1], p[1][3, :]), get_vector(M.manifold, p[2], p[2][3, :])))  
-    return Y 
+    copyto!(
+        Y,
+        X_proj_skew -
+        (s / 2) .* vcat(
+            get_vector(M.manifold, p[1], p[1][3, :]),
+            get_vector(M.manifold, p[2], p[2][3, :]),
+        ),
+    )
+    return Y
 end
 
 """
@@ -417,7 +443,7 @@ Return the array dimensions required to represent an element on the
 """
 @generated representation_size(M::EssentialManifold) = (3, 3, 2)
 
-function Base.show(io::IO, M::EssentialManifold) 
+function Base.show(io::IO, M::EssentialManifold)
     return print(io, "EssentialManifold($(M.is_signed))")
 end
 
@@ -439,12 +465,11 @@ end
 
 function vector_transport_to!(::EssentialManifold, Y, p, X, q, ::ParallelTransport)
     # group operation in the ambient group
-    pq = (q').*p
+    pq = (q') .* p
     # left translation
-    copyto!(Y, pq.*X.*pq')
+    copyto!(Y, pq .* X .* pq')
     return Y
 end
-
 
 @doc raw"""
     vert_proj(M::EssentialManifold, p, X)
@@ -457,5 +482,7 @@ where $e_z$ is the third unit vector, $X_i ∈ T_{p}\text{SO}(3)$ for $i=1,2,$ a
 pose of camera $i$ $g_i = (R_i,T'_i) ∈ \text{SE}(3)$ and $R_0 ∈ \text{SO}(3)$ such that $R_0(T'_2-T'_1) = e_z$ [^TronDaniilidis2017].
 
 """
-vert_proj(M::EssentialManifold, p, X) = vert_proj(M.manifold, p[1], X[1]) + vert_proj(M.manifold, p[2], X[2])
-vert_proj(M::Rotations{3}, p, X) = (p*get_coordinates(M, p, X))[3]
+function vert_proj(M::EssentialManifold, p, X)
+    return vert_proj(M.manifold, p[1], X[1]) + vert_proj(M.manifold, p[2], X[2])
+end
+vert_proj(M::Rotations{3}, p, X) = (p * get_coordinates(M, p, X))[3]
