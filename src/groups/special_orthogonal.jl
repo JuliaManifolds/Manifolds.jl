ebaaaf9ee5d5704bfad395e742c22eb54705a3a2
Seth Axen <seth.axen@gmail.com>
2021-04-08 18:17:11 -0700

    Forward basis functions

--- src/groups/special_orthogonal.jl
+++ src/groups/special_orthogonal.jl
@@ -20,3 +20,15 @@ default_metric_dispatch(::SpecialOrthogonal, ::EuclideanMetric) = Val(true)
 SpecialOrthogonal(n) = SpecialOrthogonal{n}()
 
 Base.show(io::IO, ::SpecialOrthogonal{n}) where {n} = print(io, "SpecialOrthogonal($(n))")
+
+for f in (:get_coordinates!, get_coordinates, :get_vector!, :get_vector)
+    @eval begin
+        function decorator_transparent_dispatch(
+            ::typeof($(f)),
+            ::SpecialOrthogonal,
+            args...,
+        )
+            return Val(:transparent)
+        end
+    end
+end
