65c4430e2aa5888d9c91bec1a2a56e96e2c638f8
Ronny Bergmann <git@ronnybergmann.net>
2021-05-16 23:57:59 +0200

    add back the implementation using NLsolve since it required Requires.jl

--- src/nlsolve.jl
+++ src/nlsolve.jl
@@ -1,3 +1,37 @@
+
+@doc raw"""
+    inverse_retract(M, p, q method::NLsolveInverseRetraction; kwargs...)
+
+Approximate the inverse of the retraction specified by `method.retraction` from `p` with
+respect to `q` on the [`Manifold`](@ref) `M` using NLsolve. This inverse retraction is
+not guaranteed to succeed and probably will not unless `q` is close to `p` and the initial
+guess `X0` is close.
+
+If the solver fails to converge, an [`OutOfInjectivityRadiusError`](@ref) is raised.
+See [`NLsolveInverseRetraction`](@ref) for configurable parameters.
+"""
+inverse_retract(::Manifold, p, q, ::NLsolveInverseRetraction; kwargs...)
+
+function inverse_retract!(M::Manifold, X, p, q, method::NLsolveInverseRetraction; kwargs...)
+    X0 = method.X0 === nothing ? zero_tangent_vector(M, p) : method.X0
+    res = _inverse_retract_nlsolve(
+        M,
+        p,
+        q,
+        method.retraction,
+        X0,
+        method.project_tangent,
+        method.project_point,
+        method.nlsolve_kwargs;
+        kwargs...,
+    )
+    if !res.f_converged
+        @debug res
+        throw(OutOfInjectivityRadiusError())
+    end
+    return copyto!(X, res.zero)
+end
+
 function _inverse_retract_nlsolve(
     M::Manifold,
     p,
