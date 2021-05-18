f1b0e1f7d2b1c565a1250e12c4202e26fbcdea98
Ronny Bergmann <git@ronnybergmann.net>
2021-05-18 15:58:48 +0200

    runs formatter.

--- assets/projection_illustration.jl
+++ assets/projection_illustration.jl
@@ -1,9 +1,17 @@
 using Manifolds, LinearAlgebra, PGFPlotsX, Colors
 
-out_file = joinpath(@__DIR__,"..","docs","src","assets","images","projection_illustration.png")
+out_file = joinpath(
+    @__DIR__,
+    "..",
+    "docs",
+    "src",
+    "assets",
+    "images",
+    "projection_illustration.png",
+)
 n = 100
 
-line_width = 1/2
+line_width = 1 / 2
 light_color = colorant"#BBBBBB"
 base_linewidth = line_width
 point_color = colorant"#0077BB" #Blue
@@ -22,13 +30,12 @@ C_to_2D(p) = [real(p), imag(p)]
 
 M = Circle(ℂ)
 
-p = (-2 + 1im) / abs(- 2 + 1im)
+p = (-2 + 1im) / abs(-2 + 1im)
 X = (imag(p) - real(p) * im)
 q = -1.3 + 1.5im
 
 s1 = project(M, q)
 s2 = project(M, p, q)
-
 
 circle = [[sin(φ), cos(φ)] for φ in range(0, 2π, length=n)]
 tp = @pgf Axis({
@@ -38,38 +45,66 @@ tp = @pgf Axis({
     xmax = 1.7,
     ymin = -.25,
     ymax = 1.4,
-    scale=1.6,
+    scale = 1.6,
 })
 
 plain_opts = @pgf {"no markers", color = light_color, line_width = base_linewidth}
 
 push!(tp, Plot(plain_opts, Coordinates(Tuple.(circle))))
-plaint_opts_a = @pgf {"only marks", mark_size = geo_marker_size/2, color = light_color}
-push!(tp, Plot(plaint_opts_a, Coordinates(Tuple.([[.0, .0],]))))
+plaint_opts_a = @pgf {"only marks", mark_size = geo_marker_size / 2, color = light_color}
+push!(tp, Plot(plaint_opts_a, Coordinates(Tuple.([[0.0, 0.0]]))))
 
-tangent_line2 = [C_to_2D(p - 0.8*X), C_to_2D(p + 1.1*X)]
+tangent_line2 = [C_to_2D(p - 0.8 * X), C_to_2D(p + 1.1 * X)]
 push!(tp, Plot(plain_opts, Coordinates(Tuple.(tangent_line2))))
 
-projection_opts2 = @pgf {"no markers", "densely dotted", mark_size = projection_marker_size, color = projection_color2}
+projection_opts2 = @pgf {
+    "no markers",
+    "densely dotted",
+    mark_size = projection_marker_size,
+    color = projection_color2,
+}
 projection_line2 = [C_to_2D(q), C_to_2D(s1)]
 push!(tp, Plot(projection_opts2, Coordinates(Tuple.(projection_line2))))
-projection_opts2_a = @pgf {"only marks", mark_size = projection_marker_size, color = projection_color2}
-push!(tp, Plot(projection_opts2_a, Coordinates(Tuple.([C_to_2D(s1),]))))
+projection_opts2_a =
+    @pgf {"only marks", mark_size = projection_marker_size, color = projection_color2}
+push!(tp, Plot(projection_opts2_a, Coordinates(Tuple.([C_to_2D(s1)]))))
 
 geo_opts2 = @pgf {"only marks", mark_size = geo_marker_size, color = geo_color}
-push!(tp, Plot(geo_opts2, Coordinates(Tuple.(C_to_2D.([p,q])))))
+push!(tp, Plot(geo_opts2, Coordinates(Tuple.(C_to_2D.([p, q])))))
 
-projection_opts3 = @pgf {"no markers", "densely dotted", mark_size = projection_marker_size, color = projection_color3}
-projection_line3 = [C_to_2D(q), C_to_2D(p+s2)]
+projection_opts3 = @pgf {
+    "no markers",
+    "densely dotted",
+    mark_size = projection_marker_size,
+    color = projection_color3,
+}
+projection_line3 = [C_to_2D(q), C_to_2D(p + s2)]
 push!(tp, Plot(projection_opts3, Coordinates(Tuple.(projection_line3))))
-projection_opts3_a = @pgf {"only marks", mark_size = projection_marker_size, color = projection_color3}
-push!(tp, Plot(projection_opts3_a, Coordinates(Tuple.([C_to_2D(p+s2),]))))
+projection_opts3_a =
+    @pgf {"only marks", mark_size = projection_marker_size, color = projection_color3}
+push!(tp, Plot(projection_opts3_a, Coordinates(Tuple.([C_to_2D(p + s2)]))))
 
-push!(tp, raw"\node[label ={[label distance=.05cm]below:{\color{gray}$q$}}] at (axis cs:"*"$(real(q)),$(imag(q))) {};")
-push!(tp, raw"\node[label ={[label distance=.05cm]left:{\color{gray}$p$}}] at (axis cs:"*"$(real(p)),$(imag(p))) {};")
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]below:{\color{gray}$q$}}] at (axis cs:" *
+    "$(real(q)),$(imag(q))) {};",
+)
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]left:{\color{gray}$p$}}] at (axis cs:" *
+    "$(real(p)),$(imag(p))) {};",
+)
 # push!(tp, raw"\node[label ={[label distance=.05cm]right:{$s_1=\mathrm{proj}_{\mathcal C}(q_2)=$\texttt{project(M,q2)}}}] at (axis cs:"*"$(real(s1)),$(imag(s1))) {};")
-push!(tp, raw"\node[label ={[label distance=.05cm]right:{\color{gray}$s_1=$\texttt{project(M,q)$\in\mathcal C$}}}] at (axis cs:"*"$(real(s1)),$(imag(s1))) {};")
-push!(tp, raw"\node[label ={[label distance=.05cm]0:{\color{gray}$s_2=$\texttt{project(M,p,q)}$\in T_{p}\mathcal C$}}] at (axis cs:"*"$(real(p+s2)),$(imag(p+s2))) {};")
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]right:{\color{gray}$s_1=$\texttt{project(M,q)$\in\mathcal C$}}}] at (axis cs:" *
+    "$(real(s1)),$(imag(s1))) {};",
+)
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]0:{\color{gray}$s_2=$\texttt{project(M,p,q)}$\in T_{p}\mathcal C$}}] at (axis cs:" *
+    "$(real(p+s2)),$(imag(p+s2))) {};",
+)
 push!(tp, raw"\node at (axis cs: 0,-.6) {\color{gray}$\mathcal C$};")
 
 pgfsave(out_file, tp, dpi=1200)
--- assets/retraction_illustration.jl
+++ assets/retraction_illustration.jl
@@ -1,9 +1,17 @@
 using Manifolds, LinearAlgebra, PGFPlotsX, Colors
 
-out_file = joinpath(@__DIR__, "..","docs","src","assets","images","retraction_illustration.png")
+out_file = joinpath(
+    @__DIR__,
+    "..",
+    "docs",
+    "src",
+    "assets",
+    "images",
+    "retraction_illustration.png",
+)
 n = 100
 
-line_width = 1/2
+line_width = 1 / 2
 light_color = colorant"#BBBBBB"
 base_linewidth = line_width
 point_color = colorant"#0077BB" #Blue
@@ -25,7 +33,7 @@ M = Circle(ℂ)
 p = (1 + 2im) / abs(1 + 2im)
 X = imag(p) - real(p) * im
 qE = exp(M, p, X)
-qP = project(M,p+X)
+qP = project(M, p + X)
 
 circle = [[sin(φ), cos(φ)] for φ in range(0, 2π, length=n)]
 tp = @pgf Axis({
@@ -35,40 +43,61 @@ tp = @pgf Axis({
     xmax = 1.7,
     ymin = -.25,
     ymax = 1.4,
-    scale=1.6,
+    scale = 1.6,
 })
 
 plain_opts = @pgf {"no markers", color = light_color, line_width = base_linewidth}
 
 push!(tp, Plot(plain_opts, Coordinates(Tuple.(circle))))
-plaint_opts_a = @pgf {"only marks", mark_size = geo_marker_size/2, color = light_color}
-push!(tp, Plot(plaint_opts_a, Coordinates(Tuple.([[.0, .0],]))))
+plaint_opts_a = @pgf {"only marks", mark_size = geo_marker_size / 2, color = light_color}
+push!(tp, Plot(plaint_opts_a, Coordinates(Tuple.([[0.0, 0.0]]))))
 
-tangent_line = [C_to_2D(p - 0.8*X), C_to_2D(p + 1.3*X)]
+tangent_line = [C_to_2D(p - 0.8 * X), C_to_2D(p + 1.3 * X)]
 push!(tp, Plot(plain_opts, Coordinates(Tuple.(tangent_line))))
 
-tangent_opts = @pgf {"no markers", "->", color = tangent_color, line_width = tangent_linewidth}
+tangent_opts =
+    @pgf {"no markers", "->", color = tangent_color, line_width = tangent_linewidth}
 tangent_vec = [C_to_2D(p), C_to_2D(p + X)]
 push!(tp, Plot(tangent_opts, Coordinates(Tuple.(tangent_vec))))
 
 geo_opts = @pgf {"no markers", "-", color = geo_color, line_width = geo_linewidth}
-geo_pts = C_to_2D.(shortest_geodesic(M, p, qE, range(0,1,length=n)))
+geo_pts = C_to_2D.(shortest_geodesic(M, p, qE, range(0, 1, length=n)))
 push!(tp, Plot(geo_opts, Coordinates(Tuple.(geo_pts))))
 geo_opts2 = @pgf {"only marks", mark_size = geo_marker_size, color = geo_color}
-push!(tp, Plot(geo_opts2, Coordinates(Tuple.([C_to_2D(p),C_to_2D(qE)]))))
+push!(tp, Plot(geo_opts2, Coordinates(Tuple.([C_to_2D(p), C_to_2D(qE)]))))
 
-
-projection_opts = @pgf {"no markers", "densely dotted", color = projection_color, line_width = base_linewidth}
-projection_line = [C_to_2D(p+X), C_to_2D(qP)]
+projection_opts = @pgf {
+    "no markers",
+    "densely dotted",
+    color = projection_color,
+    line_width = base_linewidth,
+}
+projection_line = [C_to_2D(p + X), C_to_2D(qP)]
 push!(tp, Plot(projection_opts, Coordinates(Tuple.(projection_line))))
-projection_opts_a = @pgf {"only marks", mark_size = projection_marker_size, color = projection_color}
-push!(tp, Plot(projection_opts_a, Coordinates(Tuple.([C_to_2D(qP),]))))
+projection_opts_a =
+    @pgf {"only marks", mark_size = projection_marker_size, color = projection_color}
+push!(tp, Plot(projection_opts_a, Coordinates(Tuple.([C_to_2D(qP)]))))
 
-
-push!(tp, raw"\node[label ={[label distance=.05cm]above:{\color{gray}$p$}}] at (axis cs:"*"$(real(p)),$(imag(p))) {};")
-push!(tp, raw"\node[label ={[label distance=.1cm]above:{\color{gray}$X\in T_p\mathcal C$}}] at (axis cs:"*"$(real(p+X/2)),$(imag(p+X/2))) {};")
-push!(tp, raw"\node[label ={[label distance=.05cm]left:{\color{gray}$q'=\mathrm{retr}^{\mathrm{proj}}_pX=\mathrm{proj}_{\mathcal C}(p+X)$}}] at (axis cs:"*"$(real(qP)),$(imag(qP))) {};")
-push!(tp, raw"\node[label ={[label distance=.05cm]below:{\color{gray}$q=\exp_pX$}}] at (axis cs:"*"$(real(qE)),$(imag(qE))) {};")
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]above:{\color{gray}$p$}}] at (axis cs:" *
+    "$(real(p)),$(imag(p))) {};",
+)
+push!(
+    tp,
+    raw"\node[label ={[label distance=.1cm]above:{\color{gray}$X\in T_p\mathcal C$}}] at (axis cs:" *
+    "$(real(p+X/2)),$(imag(p+X/2))) {};",
+)
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]left:{\color{gray}$q'=\mathrm{retr}^{\mathrm{proj}}_pX=\mathrm{proj}_{\mathcal C}(p+X)$}}] at (axis cs:" *
+    "$(real(qP)),$(imag(qP))) {};",
+)
+push!(
+    tp,
+    raw"\node[label ={[label distance=.05cm]below:{\color{gray}$q=\exp_pX$}}] at (axis cs:" *
+    "$(real(qE)),$(imag(qE))) {};",
+)
 
 push!(tp, raw"\node at (axis cs: 0,-.6) {\color{gray}$\mathcal C$};")
 
