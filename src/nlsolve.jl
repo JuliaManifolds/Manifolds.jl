a3b8cd11ad6fc7cbe1b7f7a5eae0c68daa6306c8
Ronny Bergmann <git@ronnybergmann.net>
2021-05-22 14:29:15 +0200

    Move a warning here from ManifoldsBase.

--- src/nlsolve.jl
+++ src/nlsolve.jl
@@ -50,6 +50,8 @@ function _inverse_retract_nlsolve(
         F .-= q
         return F
     end
+    isdefined(ManifoldsBase, :NLsolve) ||
+        @warn "To use NLsolveInverseRetraction, NLsolve must be loaded using `using NLsolve`."
     res = NLsolve.nlsolve(f!, X0; nlsolve_kwargs...)
     return res
 end
