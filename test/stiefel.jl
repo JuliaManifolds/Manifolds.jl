17ae136becadd50e0066462c000321b50ce50f19
Ronny Bergmann <git@ronnybergmann.net>
2021-06-08 16:30:42 +0200

    fix a typo.

--- test/stiefel.jl
+++ test/stiefel.jl
@@ -313,7 +313,7 @@ using Manifolds: default_metric_dispatch
         s = exp(M4, p, Z)
         Z2 = log(M4, p, s)
         @test isapprox(M4, p, Z, Z2)
-        Z3 = simliar(Z2)
+        Z3 = similar(Z2)
         log!(M4, Z3, p, s)
         @test isapprox(M4, p, Z2, Z3)
     end
