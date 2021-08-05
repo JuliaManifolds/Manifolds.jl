c56c612b39c410f77a64681eabc7eae850dfe617
Mateusz Baran <mateuszbaran89@gmail.com>
2021-08-05 19:37:17 +0200

    Covariance estimation

--- src/statistics.jl
+++ src/statistics.jl
@@ -142,6 +142,58 @@ function Base.show(io::IO, method::GeodesicInterpolationWithinRadius)
     return print(io, "GeodesicInterpolationWithinRadius($(method.radius))")
 end
 
+"""
+    Statistics.cov(
+        M::AbstractManifold,
+        x::AbstractVector;
+        basis::AbstractBasis=DefaultOrthonormalBasis(),
+        tangent_space_covariance_estimator::CovarianceEstimator=SimpleCovariance(;
+            corrected=false,
+        ),
+        mean_estimation_method::AbstractEstimationMethod=GradientDescentEstimation(),
+        inverse_retraction_method::AbstractInverseRetractionMethod=default_inverse_retraction_method(
+            M,
+        ),
+    )
+
+Estimate the covariance matrix of points `x` on manifold `M`. Since the covariance matrix
+on a manifold is a rank 2 tensor, the function returns its coefficients in basis induced by
+the given tangent space basis. See Section 5 of [^Pennec2006] for details.
+
+The mean is calculated using the specified `mean_estimation_method`, and tangent vectors at
+this mean are calculated using the provided `inverse_retraction_method`. Finally, the
+covariance matrix in the tangent plane is estimated using `tangent_space_covariance_estimator`
+(the type is defined in `StatsBase.jl` and examples of covariance estimation methods can be
+found in `CovarianceEstimation.jl`).
+
+[^Pennec2006]:
+    > X. Pennec, “Intrinsic Statistics on Riemannian Manifolds: Basic Tools for Geometric
+    > Measurements,” J Math Imaging Vis, vol. 25, no. 1, p. 127, Jul. 2006,
+    > doi: 10.1007/s10851-006-6228-4.
+"""
+function Statistics.cov(
+    M::AbstractManifold,
+    x::AbstractVector;
+    basis::AbstractBasis=DefaultOrthonormalBasis(),
+    tangent_space_covariance_estimator::CovarianceEstimator=SimpleCovariance(;
+        corrected=true,
+    ),
+    mean_estimation_method::AbstractEstimationMethod=GradientDescentEstimation(),
+    inverse_retraction_method::AbstractInverseRetractionMethod=default_inverse_retraction_method(
+        M,
+    ),
+)
+    μ = mean(M, x, mean_estimation_method)
+    Xs_at_μ = map(p -> inverse_retract(M, μ, p, inverse_retraction_method), x)
+    Xcs = map(X -> get_coordinates(M, μ, X, basis), Xs_at_μ)
+    return cov(
+        tangent_space_covariance_estimator,
+        reduce(hcat, Xcs);
+        mean=zero(Xcs[1]),
+        dims=2,
+    )
+end
+
 @doc raw"""
     mean(M::AbstractManifold, x::AbstractVector[, w::AbstractWeights]; kwargs...)
 
