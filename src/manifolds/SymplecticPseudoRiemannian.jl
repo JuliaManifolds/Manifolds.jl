fd71ba3834e81bae0eaeaf04bc3f313b72b0ade5
Johannes Voll Kolstø <johannesvk@protonmail.com>
2021-09-29 13:46:06 +0200

    Removed Debugging Print-statement.

--- src/manifolds/SymplecticPseudoRiemannian.jl
+++ src/manifolds/SymplecticPseudoRiemannian.jl
@@ -5,7 +5,6 @@ struct PseudoRiemannianMetric <: AbstractMetric
 end
 
 function exp!(::MetricManifold{ℝ, <:AbstractSymplectic{n, ℝ}, PseudoRiemannianMetric}, q, p, X) where {n, ℝ}
-    println("Called 'exp!()' for PseudoRiemannianMetric.")
     p_inv = inv(p)
     q .= p*LinearAlgebra.exp(p_inv * X)
 end
