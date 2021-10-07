db2817bbbecd7f4b7174b8531f8ba7b428415b0f
Mateusz Baran <mateuszbaran89@gmail.com>
2021-10-07 21:08:08 +0200

    Remove `ProductArray`

--- test/groups/product_group.jl
+++ test/groups/product_group.jl
@@ -23,65 +23,12 @@ include("group_utils.jl")
     tuple_pts = [(exp(Rn, x, hat(Rn, x, ωi)), ti) for (ωi, ti) in zip(ω, t)]
     tuple_v = (hat(Rn, x, [1.0, 0.5, -0.5]), [-1.0, 2.0])
     eA = [x, zeros(2)]
-    shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
-    e = Manifolds.prod_point(shape_se, eA...)
 
     @testset "Product Identity" begin
         i = Identity(G)
         @test submanifold_components(G, i) == (Identity(SOn), Identity(Tn))
         @test submanifold_component(G, i, 1) == Identity(SOn)
         @test submanifold_component(G, i, 2) == Identity(Tn)
-    end
-
-    @testset "product point" begin
-        reshapers = (Manifolds.ArrayReshaper(), Manifolds.StaticReshaper())
-        for reshaper in reshapers
-            shape_se = Manifolds.ShapeSpecification(reshaper, M.manifolds...)
-            pts = [Manifolds.prod_point(shape_se, tp...) for tp in tuple_pts]
-            v_pts = [Manifolds.prod_point(shape_se, tuple_v...)]
-            @test compose(G, pts[1], Identity(G)) == pts[1]
-            @test compose(G, Identity(G), pts[1]) == pts[1]
-            test_group(G, pts, v_pts, v_pts; test_diff=true)
-            @test isapprox(
-                G,
-                Identity(G),
-                exp_lie(G, v_pts[1]),
-                Manifolds.prod_point(
-                    shape_se,
-                    exp_lie(SOn, v_pts[1].parts[1]),
-                    exp_lie(Tn, v_pts[1].parts[2]),
-                ),
-            )
-            @test isapprox(
-                G,
-                Identity(G),
-                log_lie(G, pts[1]),
-                Manifolds.prod_point(
-                    shape_se,
-                    log_lie(SOn, pts[1].parts[1]),
-                    log_lie(Tn, pts[1].parts[2]),
-                ),
-            )
-            X = log_lie(G, pts[1])
-            Z = zero_vector(G, pts[1])
-            log_lie!(G, Z, pts[1])
-            @test isapprox(G, pts[1], X, Z)
-            p = exp_lie(G, X)
-            q = identity_element(G)
-            @test is_identity(G, q)
-            @test isapprox(G, q, Identity(G))
-            @test isapprox(G, Identity(G), q)
-            exp_lie!(G, q, X)
-            @test isapprox(G, p, q)
-            log_lie!(G, Z, Identity(G))
-            @test isapprox(G, Identity(G), Z, zero_vector(G, identity_element(G)))
-            @test isapprox(
-                G,
-                Identity(G),
-                log_lie(G, Identity(G)),
-                zero_vector(G, identity_element(G)),
-            )
-        end
     end
 
     @testset "product repr" begin
--- test/groups/semidirect_product_group.jl
+++ test/groups/semidirect_product_group.jl
@@ -17,11 +17,8 @@ include("group_utils.jl")
     ts1 = Vector{Float64}.([1:2, 2:3, 3:4])
     ts2 = Vector{Float64}.([1:2, 2:3, 3:4]) .* 10
     tuple_pts = [zip(ts1, ts2)...]
-    tuple_v = ([-1.0, 1.0], [2.0, 3.0])
 
-    shape_se = Manifolds.ShapeSpecification(Manifolds.ArrayReshaper(), M.manifolds...)
-    pts = [Manifolds.prod_point(shape_se, tp...) for tp in tuple_pts]
-    v_pts = [Manifolds.prod_point(shape_se, tuple_v...)]
+    pts = [ProductRepr(tp...) for tp in tuple_pts]
 
     X = log(G, pts[1], pts[1])
     Y = zero_vector(G, pts[1])
