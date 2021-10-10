b5beecead957a573faa079aa8b35a82667972ccb
Ronny Bergmann <git@ronnybergmann.net>
2021-10-10 14:34:08 +0200

    Update src/manifolds/SymmetricPositiveDefinite.jl
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/manifolds/SymmetricPositiveDefinite.jl
+++ src/manifolds/SymmetricPositiveDefinite.jl
@@ -152,6 +152,8 @@ end
 project a matrix from the embedding onto the tangent space $T_p\mathcal P(n)$ of the
 [SymmetricPositiveDefinite](@ref) matrices, i.e. the set of symmetric matrices.
 """
+project(::SymmetricPositiveDefinite, p, X)
+
 project!(::SymmetricPositiveDefinite, Y, p, X) = (Y .= Symmetric((X + X') / 2))
 
 @doc raw"""
