1f82b1cdd637a0eb7b4ffa04837b66dc7e9aacbc
Johannes Voll Kolstø <johannesvk@protonmail.com>
2021-11-03 15:56:47 +0100

    Removed Unnecessary variations of the inner product function.

--- src/manifolds/SymplecticStiefel.jl
+++ src/manifolds/SymplecticStiefel.jl
@@ -1,8 +1,8 @@
 @doc raw"""
 
-The Symplectic Stiefel manifold. Each element represent a Symplectic Subspace of ``ℝ^{2n × 2k}``. 
+The Symplectic Stiefel manifold. Each element represent a Symplectic Subspace of ``ℝ^{2n × 2k}``.
 """
-struct SymplecticStiefel{n, k, 𝔽} <: AbstractEmbeddedManifold{𝔽, DefaultIsometricEmbeddingType} 
+struct SymplecticStiefel{n, k, 𝔽} <: AbstractEmbeddedManifold{𝔽, DefaultIsometricEmbeddingType}
 end
 
 #TODO: Implement exponential mapping from Bendokat-Zimmermann.
@@ -10,7 +10,7 @@ end
 @doc """
     You are given a manifold of embedding dimension 2n × 2p.
 """
-SymplecticStiefel(n::Int, k::Int, field::AbstractNumbers=ℝ) = begin 
+SymplecticStiefel(n::Int, k::Int, field::AbstractNumbers=ℝ) = begin
     SymplecticStiefel{n, k, field}()
 end
 Base.show(io::IO, ::SymplecticStiefel{n, k}) where {n, k} = print(io, "SymplecticStiefel{$(2n), $(2k)}()")
@@ -18,24 +18,24 @@ Base.show(io::IO, ::SymplecticStiefel{n, k}) where {n, k} = print(io, "Symplecti
 decorated_manifold(::SymplecticStiefel{n, k, ℝ}) where {n, k} = Euclidean(2n, 2k; field=ℝ)
 
 @doc raw"""
-    manifold_dimension(::SymplecticStiefel{n, k})    
+    manifold_dimension(::SymplecticStiefel{n, k})
 
 As shown in proposition 3.1 in Bendokat-Zimmermann.
 """
-manifold_dimension(::SymplecticStiefel{n, k}) where {n, k} = (4n -2k + 1)*k 
+manifold_dimension(::SymplecticStiefel{n, k}) where {n, k} = (4n -2k + 1)*k
 
 function check_point(M::SymplecticStiefel{n, k}, p; kwargs...) where {n, k}
     abstract_embedding_type = supertype(typeof(M))
-    
+
     mpv = invoke(check_point, Tuple{abstract_embedding_type, typeof(p)}, M, p; kwargs...)
     mpv === nothing || return mpv
-    
+
     # Perform check that the matrix lives on the real symplectic manifold:
     expected_zero = norm(inv(M, p) * p - I)
     if !isapprox(expected_zero, 0; kwargs...)
         return DomainError(
             expected_zero,
-            ("The point p does not lie on $(M) because its symplectic" 
+            ("The point p does not lie on $(M) because its symplectic"
            * " inverse composed with itself is not the identity. $(p)")
         )
     end
@@ -81,25 +81,6 @@ end
 
 Based on the inner product in Proposition 3.10 of Benodkat-Zimmermann.
 """
-function inner_3(::SymplecticStiefel{n, k}, p, X, Y) where {n, k}
-    Q = SymplecticMatrix(p, X, Y)
-    I = UniformScaling(2n)
-
-    # Saves time compared to recomputing (p' * p) and solving a linear system twice:
-    inv_pT_p = inv(p' * p) # 𝞞((2k)^3)?
-
-    return tr(X' * (I - (1/2) * Q' * p * inv_pT_p * p' * Q) * Y * inv_pT_p) 
-end
-
-function inner_2(::SymplecticStiefel{n, k}, p, X, Y) where {n, k}
-    Q = SymplecticMatrix(p, X, Y)
-    I = UniformScaling(2n)
-    # p_Tp = lu(p' * p)
-    # inner_matrix = (I - (1/2) * Q' * p * (p_Tp \ (p')) * Q) 
-    # return tr(X' * (I - (1/2) * Q' * p * (p_Tp \ (p')) * Q) * (Y / p_Tp))
-    return tr(X' * (I - (1/2) * Q' * p * ((p' * p) \ (p')) * Q) * (Y / (p' * p)))
-end
-
 function inner(::SymplecticStiefel{n, k}, p, X, Y) where {n, k}
     # This version is Benchmarked to use only two thirds the time of the previous inner-implementation.
     # We are only finding the LU-factorization of the (2k × 2k) matrix (p' * p).
@@ -112,65 +93,17 @@ function inner(::SymplecticStiefel{n, k}, p, X, Y) where {n, k}
     return tr(X' * (I - (1/2) * Q' * p * (p_Tp \ (p')) * Q) * (Y / p_Tp))
 end
 
-function inner_mul(::SymplecticStiefel{n, k}, p, X, Y) where {n, k}
-    # Not faster, and does not save allocations...
-
-    Q = SymplecticMatrix(p, X, Y)
-    I = UniformScaling(2n)
-    # Perform LU-factorization before multiplication by inverse:
-    p_Tp = lu(p' * p)
-
-    # temp_2n_2k = similar(p)
-    temp_2k_2k = similar(p, (2k, 2k))
-    temp_2n_2n = similar(p, (2n, 2n))
-    temp_2k_2n = similar(p, (2k, 2n))
-
-    # End goal:
-    # tr(X' * (I - (1/2) * Q' * p * (p_Tp \ (p')) * Q) * (Y / p_Tp))
-
-    # Compute the inner matrix in-place as much as possible: 
-    # First: (p_Tp \ p') -> temp_2k_2n
-    ldiv!(temp_2k_2n, p_Tp, p')
-
-    # Then: p * (p_Tp \ p') -> temp_2n_2n
-    mul!(temp_2n_2n, p, temp_2k_2n)
-
-    # (Q'/2)* p * (p_Tp \ p') * Q -> temp_2n_2n
-    lmul!((1/2)*Q', temp_2n_2n)
-    rmul!(temp_2n_2n, Q)
-
-    inner_matrix = I + lmul!(-1, temp_2n_2n) 
-
-    # Left-multiply:
-    # Multiply X' * inner_matrix from the left -> in 'temp_2k_2n':
-    mul!(temp_2k_2n, X', inner_matrix)
-
-    # Hard to do:
-    # Y * (p_Tp)^{1} -> temp_2n_2k:
-    
-    # Go directly to:
-    # (X' * inner_matrix) * (Y * (p_Tp)^{1}) -> temp_2k_2k:
-    mul!(temp_2k_2k, temp_2k_2n, Y / p_Tp)
-    
-    return tr(temp_2k_2k)    
-end
-
-
-function Base.inv(::SymplecticStiefel, p) 
+function Base.inv(::SymplecticStiefel, p)
     Q = SymplecticMatrix(p)
-    return Q' * p' * Q 
+    return Q' * p' * Q
 end
 
-#= 
-Bad Idea: Nothing is square...
-function inv!(::SymplecticStiefel, q, p) 
-    Q = SymplecticMatrix(p)
-    mul!(q, p', Q)
-    lmul!(Q', q) 
-    return q
-end
-=#
 
+@doc raw"""
+    change_representer!(::SymplecticStiefel{n, k}, Y, ::EuclideanMetric, p, X)
+
+Calculated myself, but tailored to the right-invariant inner product of Bendokat-Zimmermann
+"""
 function change_representer!(::SymplecticStiefel{n, k}, Y, ::EuclideanMetric, p, X) where {n, k}
     # Quite an ugly expression: Have checked and it seems to be working.
     Q = SymplecticMatrix(p, X)
@@ -207,7 +140,7 @@ end
 
 
 @doc raw"""
-    π(::SymplecticStiefel{n, k}, p) where {n, k}    
+    π(::SymplecticStiefel{n, k}, p) where {n, k}
 
 Define the canonical projection from ``\operatorname{Sp}(2n, 2n)`` onto ``\operatorname{SpSt}(2n, 2k)``,
 by projecting onto the first ``k`` columns, and the ``n + 1``'th column onto the ``n + k``'th columns.
@@ -222,7 +155,7 @@ function canonical_projection(M::SymplecticStiefel{n, k}, p) where {n, k}
 end
 
 function canonical_projection!(::SymplecticStiefel{n, k}, p_SpSt, p) where {n, k}
-    p_SpSt[:, (1:k)] .= p[:, (1:k)]; 
+    p_SpSt[:, (1:k)] .= p[:, (1:k)];
     p_SpSt[:, (k+1:2k)] .= p[:, (n+1:n+k)]
     return p_SpSt
 end
@@ -231,7 +164,7 @@ end
     retract!(::SymplecticStiefel, q, p, X, ::CayleyRetraction)
 
 Define the Cayley retraction on the Symplectic Stiefel manifold.
-Reduced requirements down to inverting an (2k × 2k) matrix. 
+Reduced requirements down to inverting an (2k × 2k) matrix.
 Formula due to Bendokat-Zimmermann Proposition 5.2.
 
 # TODO: Add formula from Bendokat-Zimmermann.
@@ -254,7 +187,7 @@ ManifoldsBase.default_retraction_method(::SymplecticStiefel) = CayleyRetraction(
 Compute the Cayley Inverse Retraction as in proposition 5.3 of Bendorkat & Zimmermann[^Bendokat2021].
 
 First, recall the definition the standard symplectic matrix
-``Q_{2n} = 
+``Q_{2n} =
 \begin{bmatrix}
  0    & I_n \\
 -I_n  & 0
@@ -271,7 +204,7 @@ inverse cayley retraction as long as the following matrices exist.
 
 Finally, definition of the inverse cayley retration at ``p`` applied to ``q`` is
 ````math
-\mathcal{L}_p^{\operatorname{Sp}}(q) = 2p\bigl(V - U\bigr) + 2\bigl((p + q)U - p\bigr) ∈ T_p\operatorname{Sp}(2n).   
+\mathcal{L}_p^{\operatorname{Sp}}(q) = 2p\bigl(V - U\bigr) + 2\bigl((p + q)U - p\bigr) ∈ T_p\operatorname{Sp}(2n).
 ````
 
 [Bendokat2021]
@@ -300,11 +233,11 @@ end
 
 function grad_euclidian_to_manifold(::SymplecticStiefel, p, ∇f_euc)
     Q = SymplecticMatrix(p, ∇f_euc)
-    return (∇f_euc * p'  .+ Q * p * (∇f_euc)' * Q) * p     
+    return (∇f_euc * p'  .+ Q * p * (∇f_euc)' * Q) * p
 end
 
 function grad_euclidian_to_manifold!(::SymplecticStiefel, ∇f_man, p, ∇f_euc)
     Q = SymplecticMatrix(p, ∇f_euc)
-    ∇f_man .= (∇f_euc * p' .+ Q * p * (∇f_euc)' * Q) * p    
+    ∇f_man .= (∇f_euc * p' .+ Q * p * (∇f_euc)' * Q) * p
     return ∇f_man
 end
