21c6870a547989447b7372e0a73596e0bb0b204e
Ronny Bergmann <git@ronnybergmann.net>
2022-01-26 22:39:47 +0100

    Fix a typo in sphere.

--- test/manifolds/sphere.jl
+++ test/manifolds/sphere.jl
@@ -14,7 +14,7 @@ using ManifoldsBase: TFVector
         @test injectivity_radius(M, ProjectionRetraction()) == π / 2
         @test base_manifold(M) === M
         @test is_default_metric(M, EuclideanMetric())
-        @test !is_defaultMetric(M, LinearAffineMetric())
+        @test !is_default_metric(M, LinearAffineMetric())
         @test !is_point(M, [1.0, 0.0, 0.0, 0.0])
         @test !is_vector(M, [1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0])
         @test_throws DomainError is_point(M, [2.0, 0.0, 0.0], true)
