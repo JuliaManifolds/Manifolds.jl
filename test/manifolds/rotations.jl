673e548b3c4bd3e8a382eddc2affafb180cde102
Mateusz Baran <mateuszbaran89@gmail.com>
2022-05-15 12:16:00 +0200

    Diagonalizing ONB for rotations (#472)
    * diagonalizing ONB for rotations
    
    * one more test
    
    * fixing the case with _ev_zero
    
    * fix diagonalizing ONB even more
    
    * fix again

--- test/manifolds/rotations.jl
+++ test/manifolds/rotations.jl
@@ -103,6 +103,20 @@ include("../utils.jl")
             ptd = Manifolds.normal_rotation_distribution(SOn, Matrix(1.0I, n, n), 1.0)
             tvd = Manifolds.normal_tvector_distribution(SOn, Matrix(1.0I, n, n), 1.0)
             pts = [rand(ptd) for _ in 1:3]
+            diag_basis_1 = if n == 3
+                DiagonalizingOrthonormalBasis(
+                    [
+                        0.0 0.24800271831269094 0.30019597622794186
+                        -0.24800271831269094 0.0 -0.5902347224334308
+                        -0.30019597622794186 0.5902347224334308 0.0
+                    ],
+                )
+            else
+                DiagonalizingOrthonormalBasis(rand(SOn; vector_at=pts[1]))
+            end
+            diag_basis_2 = DiagonalizingOrthonormalBasis(
+                hat(SOn, pts[1], [1.0, zeros(manifold_dimension(SOn) - 1)...]),
+            )
             test_manifold(
                 SOn,
                 pts;
@@ -115,6 +129,7 @@ include("../utils.jl")
                 point_distributions=[ptd],
                 tvector_distributions=[tvd],
                 basis_types_to_from=basis_types,
+                basis_types_vecs=(diag_basis_1, diag_basis_2),
                 exp_log_atol_multiplier=250,
                 retraction_atol_multiplier=12,
                 test_inplace=true,
