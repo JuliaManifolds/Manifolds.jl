8dc24d9d2f70e542988fa6a00c5db2873abe5d25
Mateusz Baran <mateuszbaran89@gmail.com>
2022-05-20 10:01:12 +0200

    docs and improved implementations of exp and log on Heisenberg group

--- src/groups/heisenberg.jl
+++ src/groups/heisenberg.jl
@@ -192,11 +192,32 @@ end
     exp(M::HeisenbergGroup, p, X)
 
 Exponential map on the [`HeisenbergGroup`](@ref) `M` with the left-invariant metric.
+The expression reads
+```math
+\exp_{\begin{bmatrix} 1 & \mathbf{a}_p & c_p \\
+\mathbf{0} & I_n & \mathbf{b}_p \\
+0 & \mathbf{0} & 1 \end{bmatrix}}\left(\begin{bmatrix} 0 & \mathbf{a}_X & c_X \\
+\mathbf{0} & 0_n & \mathbf{b}_X \\
+0 & \mathbf{0} & 0 \end{bmatrix}\right) =
+\begin{bmatrix} 1 & \mathbf{a}_p + \mathbf{a}_X & c_p + c_X + \mathbf{a}_X⋅\mathbf{b}_X/2 + \mathbf{a}_p⋅\mathbf{b}_X \\
+\mathbf{0} & I_n & \mathbf{b}_p + \mathbf{b}_X \\
+0 & \mathbf{0} & 1 \end{bmatrix}
+```
+where ``I_n`` is the ``n×n`` identity matrix, ``0_n`` is the ``n×n`` zero matrix
+and ``\mathbf{a}⋅\mathbf{b}`` is dot product of vectors.
 """
 exp(M::HeisenbergGroup, p, X)
 
 function exp!(M::HeisenbergGroup{n}, q, p, X) where {n}
-    q .= p * exp_lie(M, X)
+    copyto!(q, I)
+    a_p_view = _heisenberg_a_view(M, p)
+    b_p_view = _heisenberg_b_view(M, p)
+    a_X_view = _heisenberg_a_view(M, X)
+    b_X_view = _heisenberg_b_view(M, X)
+    q[1, 2:(n + 1)] .= a_p_view .+ a_X_view
+    q[2:(n + 1), n + 2] .= b_p_view .+ b_X_view
+    q[1, n + 2] =
+        p[1, n + 2] + X[1, n + 2] + dot(a_X_view, b_X_view) / 2 + dot(a_p_view, b_X_view)
     return q
 end
 
@@ -221,12 +242,32 @@ end
     log(G::HeisenbergGroup, p, q)
 
 Compute the logarithmic map on the [`HeisenbergGroup`](@ref) group.
-
+The formula reads
+```math
+\log_{\begin{bmatrix} 1 & \mathbf{a}_p & c_p \\
+\mathbf{0} & I_n & \mathbf{b}_p \\
+0 & \mathbf{0} & 1 \end{bmatrix}}\left(\begin{bmatrix} 1 & \mathbf{a}_q & c_q \\
+\mathbf{0} & I_n & \mathbf{b}_q \\
+0 & \mathbf{0} & 1 \end{bmatrix}\right) =
+\begin{bmatrix} 0 & \mathbf{a}_q - \mathbf{a}_p & c_q - c_p + \mathbf{a}_p⋅\mathbf{b}_p - \mathbf{a}_q⋅\mathbf{b}_q - (\mathbf{a}_q - \mathbf{a}_p)⋅(\mathbf{b}_q - \mathbf{b}_p) / 2 \\
+\mathbf{0} & 0_n & \mathbf{b}_q - \mathbf{b}_p \\
+0 & \mathbf{0} & 0 \end{bmatrix}
+```
+where ``I_n`` is the ``n×n`` identity matrix, ``0_n`` is the ``n×n`` zero matrix
+and ``\mathbf{a}⋅\mathbf{b}`` is dot product of vectors.
 """
 log(::HeisenbergGroup, p, q)
 
-function log!(G::HeisenbergGroup{n}, X, p, q) where {n}
-    X .= log_lie(G, p \ q)
+function log!(M::HeisenbergGroup{n}, X, p, q) where {n}
+    fill!(X, 0)
+    a_p_view = _heisenberg_a_view(M, p)
+    b_p_view = _heisenberg_b_view(M, p)
+    a_q_view = _heisenberg_a_view(M, q)
+    b_q_view = _heisenberg_b_view(M, q)
+    X[1, 2:(n + 1)] .= a_q_view .- a_p_view
+    X[2:(n + 1), n + 2] .= b_q_view .- b_p_view
+    pinvq_c = dot(a_p_view, b_p_view) - p[1, n + 2] + q[1, n + 2] - dot(a_p_view, b_q_view)
+    X[1, n + 2] = pinvq_c - dot(a_q_view - a_p_view, b_q_view - b_p_view) / 2
     return X
 end
 
@@ -238,7 +279,8 @@ The formula reads
 ```math
 \log\left(\begin{bmatrix} 1 & \mathbf{a} & c \\
 \mathbf{0} & I_n & \mathbf{b} \\
-0 & \mathbf{0} & 1 \end{bmatrix}\right) = \begin{bmatrix} 0 & \mathbf{a} & c - \mathbf{a}⋅\mathbf{b}/2 \\
+0 & \mathbf{0} & 1 \end{bmatrix}\right) =
+\begin{bmatrix} 0 & \mathbf{a} & c - \mathbf{a}⋅\mathbf{b}/2 \\
 \mathbf{0} & 0_n & \mathbf{b} \\
 0 & \mathbf{0} & 0 \end{bmatrix}
 ```
