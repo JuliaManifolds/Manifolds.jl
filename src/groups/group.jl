4ed4b68440500e179d7d86fc2df3e9b456d695b7
Dehann Fourie <6412556+dehann@users.noreply.github.com>
2022-06-22 23:22:40 -0700

    typo, docs hat

--- src/groups/group.jl
+++ src/groups/group.jl
@@ -528,7 +528,7 @@ transpose(e::Identity) = e
 @doc raw"""
     hat(M::AbstractDecoratorManifold{𝔽,O}, ::Identity{O}, Xⁱ) where {𝔽,O<:AbstractGroupOperation}
 
-Given a basis $e_i$ on the tangent space at a the [`Identity`}(@ref) and tangent
+Given a basis $e_i$ on the tangent space at a the [`Identity`](@ref) and tangent
 component vector ``X^i``, compute the equivalent vector representation
 ``X=X^i e_i**, where Einstein summation notation is used:
 
