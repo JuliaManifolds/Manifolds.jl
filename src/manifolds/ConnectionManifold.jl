1cf5c36f6fd7cd5c80bd6358cf0dc9bc8f2691f4
Mateusz Baran <mateuszbaran89@gmail.com>
2022-07-31 16:12:17 +0200

    switch to riemann_tensor in ManifoldsBase

--- src/manifolds/ConnectionManifold.jl
+++ src/manifolds/ConnectionManifold.jl
@@ -336,24 +336,6 @@ end
 @trait_function riemann_tensor(M::AbstractDecoratorManifold, p, B::AbstractBasis; kwargs...)
 
 @doc raw"""
-    riemann_tensor(M::AbstractManifold, p, X, Y, Z)
-
-Compute the value of the Riemann tensor ``R(X_f,Y_f)Z_f`` at point `p`, where
-``X_f``, ``Y_f`` and ``Z_f`` are vector fields defined by parallel transport of,
-respectively, `X`, `Y` and `Z` to the desired point. All computations are performed
-using the connection associated to manifold `M`.
-
-The formula reads ``R(X_f,Y_f)Z_f = \nabla_X\nabla_Y Z - \nabla_Y\nabla_X Z - \nabla_{[X, Y]}Z``,
-where ``[X, Y]`` is the Lie bracket of vector fields.
-
-Note that some authors define this quantity with inverse sign.
-"""
-function riemann_tensor(M::AbstractManifold, p, X, Y, Z)
-    Xresult = allocate_result(M, X)
-    return riemann_tensor!(M, Xresult, p, X, Y, Z)
-end
-
-@doc raw"""
     solve_exp_ode(
         M::AbstractConnectionManifold,
         p,
