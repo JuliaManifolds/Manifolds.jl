f859641e9bfa43911b6d60a8dd0eec03e121777a
jondeuce <20175323+jondeuce@users.noreply.github.com>
2022-09-11 07:46:06 -0700

    Update docstring to match implementation (#530)
    Hi there, I found what I believe to be a small typo in the doc string of the `project!` method for general unitary matrices, as the implementation projects `p \ X` onto the skew-symmetric matrices, and not `p * X`.

--- src/manifolds/GeneralUnitaryMatrices.jl
+++ src/manifolds/GeneralUnitaryMatrices.jl
@@ -669,7 +669,7 @@ to the tangent space of `M` at `p`,
 and change the representer to use the corresponding Lie algebra, i.e. we compute
 
 ```math
-    \operatorname{proj}_p(X) = \frac{pX-(pX)^{\mathrm{T}}}{2},
+    \operatorname{proj}_p(X) = \frac{p^{\mathrm{H}} X - (p^{\mathrm{H}} X)^{\mathrm{H}}}{2},
 ```
 """
 project(::GeneralUnitaryMatrices, p, X)
