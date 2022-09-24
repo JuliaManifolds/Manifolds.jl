3307e903a10fb0a469d49eb252c03d7681e8962c
Seth Axen <seth@sethaxen.com>
2022-09-24 23:23:07 +0200

    Move code to its own shooting method

--- src/shooting.jl
+++ src/shooting.jl
@@ -27,38 +27,60 @@ function inverse_retract!(
     q,
     method::ShootingInverseRetraction,
 )
+    _shooting!(
+        M, X, p, q;
+        max_iterations=method.max_iterations,
+        tolerance=method.tolerance,
+        num_transport_points=method.num_transport_points,
+        retraction=method.retraction,
+        vector_transport=method.vector_transport,
+    )
+    return X
+end
+
+function _shooting!(
+    M,
+    X,
+    p,
+    q;
+    max_iterations,
+    tolerance,
+    num_transport_points,
+    retraction,
+    vector_transport,
+)
     T = real(Base.promote_eltype(X, p, q))
-    ts = range(zero(T), one(T); length=method.num_transport_points)
+    ts = range(zero(T), one(T); length=num_transport_points)
     X .= q .- p
     gap = norm(X)
     project!(M, X, p, X)
-    gap < method.tolerance && return X
+    gap < tolerance && return X
     rmul!(X, gap / norm(X))
     i = 1
     Xˢ = allocate(X)
     Xˢnew = allocate(Xˢ)
     retr_tX = allocate_result(M, retract, p, X)
     retr_tX_new = allocate_result(M, retract, p, X)
-    while (gap > method.tolerance) && (i < method.max_iterations)
-        retract!(M, retr_tX, p, X, method.retraction)
+    while (gap > tolerance) && (i < max_iterations)
+        retract!(M, retr_tX, p, X, retraction)
         Xˢ .= retr_tX .- q
         gap = norm(Xˢ)
         project!(M, Xˢ, retr_tX, Xˢ)
         rmul!(Xˢ, gap / norm(Xˢ))
         for t in reverse(ts)[2:(end - 1)]
-            retract!(M, retr_tX_new, p, t * X, method.retraction)
+            retract!(M, retr_tX_new, p, t * X, retraction)
             vector_transport_to!(
                 M,
                 Xˢnew,
                 retr_tX,
                 Xˢ,
                 retr_tX_new,
-                method.vector_transport,
+                vector_transport,
             )
             retr_tX, retr_tX_new = retr_tX_new, retr_tX
             Xˢ, Xˢnew = Xˢnew, Xˢ
         end
-        vector_transport_to!(M, Xˢnew, retr_tX, Xˢ, p, method.vector_transport)
+        vector_transport_to!(M, Xˢnew, retr_tX, Xˢ, p, vector_transport)
         X .-= Xˢnew
         i += 1
     end
