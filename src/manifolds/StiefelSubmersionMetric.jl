62829bca95515462c466c72e31d877699a20c015
Seth Axen <seth@sethaxen.com>
2022-09-24 15:47:54 +0200

    Initial implementation of exp!

--- src/manifolds/StiefelSubmersionMetric.jl
+++ src/manifolds/StiefelSubmersionMetric.jl
@@ -18,6 +18,41 @@ struct StiefelSubmersionMetric{T<:Real} <: RiemannianMetric
 end
 
 
+function exp!(
+    M::MetricManifold{ℝ,Stiefel{n,k,ℝ},<:StiefelSubmersionMetric},
+    q,
+    p,
+    X,
+) where {n,k}
+    α = metric(M).α
+    # TODO:
+    # - dispatch to exp! for α = -1/2 and α = 0 for efficiency
+    # - reduce allocations
+    # - handle rank-deficient QB
+    if k ≤ div(n, 2)
+        # eq. 11
+        A = p'X
+        Q, B = qr(X - p * A)
+        copyto!(
+            q,
+            [p Matrix(Q)] *
+            exp([A/(α + 1) -B'; B zero(B)])[:, 1:k] *
+            exp(A * (α / (α + 1))),
+        )
+    elseif n == k
+        copyto!(q, exp((X * p') / (α + 1)) * p * exp((p' * X) * (α / (α + 1))))
+    else  # n/2 < k < n
+        # eq. 8
+        A = p' * X
+        C = X * p'
+        copyto!(
+            q,
+            exp((-(2α + 1) / (α + 1)) * (p * A * p') + C - C') * p * exp((α / (α + 1)) * A),
+        )
+    end
+    return q
+end
+
 @doc raw"""
     inner(M::MetricManifold{ℝ, Stiefel{n,k,ℝ}, X, <:StiefelSubmersionMetric}, p, X, Y)
 
