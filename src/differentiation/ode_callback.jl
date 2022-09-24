15aeec306408d3dd9c21e9fc88fe073a09541c14
Mateusz Baran <mateuszbaran89@gmail.com>
2022-09-24 15:14:30 +0200

    faster solver

--- src/differentiation/ode_callback.jl
+++ src/differentiation/ode_callback.jl
@@ -45,9 +45,24 @@ function (scs::StitchedChartSolution)(t::AbstractArray)
     return map(scs, t)
 end
 
+"""
+    solve_chart_exp_ode(
+        M::AbstractManifold,
+        a,
+        X,
+        A::AbstractAtlas,
+        i0;
+        solver=AutoVern9(Rodas5()),
+        final_time=1.0,
+        kwargs...,
+    )
+
+Solve geodesic ODE on manifold `M` from point of coordinates `a` in chart `i0` from atlas
+`A` in direction of coordinates `X` in induced basis.
+"""
 function solve_chart_exp_ode(
     M::AbstractManifold,
-    p,
+    a,
     X,
     A::AbstractAtlas,
     i0;
@@ -55,12 +70,12 @@ function solve_chart_exp_ode(
     final_time=1.0,
     kwargs...,
 )
-    u0 = ArrayPartition(p, X)
+    u0 = ArrayPartition(a, X)
     cur_i = i0
     # callback stops solver when we get too close to chart boundary
     cb = FunctionCallingCallback(maybe_switch_chart; func_start=false)
     retcode = :Terminated
-    init_time = 0.0
+    init_time = zero(final_time)
     sols = StitchedChartSolution(M, A, typeof(i0))
     while retcode === :Terminated && init_time < final_time
         B = induced_basis(M, A, cur_i)
@@ -69,16 +84,21 @@ function solve_chart_exp_ode(
             ODEProblem(chart_exp_problem, u0, (init_time, final_time), params; callback=cb)
         sol = solve(prob, solver; kwargs...)
         retcode = sol.retcode
-        init_time = sol.t[end]
+        init_time = sol.t[end]::typeof(final_time)
         push!(sols.sols, (sol, cur_i))
         # here we switch charts
-        new_i = get_chart_index(M, A, cur_i, sol.u[end].x[1])
-        new_p0 = transition_map(M, A, cur_i, new_i, sol.u[end].x[1])
+        a_final = sol.u[end].x[1]::typeof(a)
+        new_i = get_chart_index(M, A, cur_i, a_final)
+        new_p0 = transition_map(M, A, cur_i, new_i, a_final)
         new_B = induced_basis(M, A, new_i)
-        p_final = get_point(M, A, cur_i, sol.u[end].x[1])
-        new_X0 =
-            get_coordinates(M, p_final, get_vector(M, p_final, sol.u[end].x[2], B), new_B)
-        u0 = ArrayPartition(new_p0, new_X0)
+        p_final = get_point(M, A, cur_i, a_final)
+        new_X0 = get_coordinates(
+            M,
+            p_final,
+            get_vector(M, p_final, sol.u[end].x[2]::typeof(X), B),
+            new_B,
+        )
+        u0 = ArrayPartition(new_p0, new_X0)::typeof(u0)
         cur_i = new_i
     end
     return sols
