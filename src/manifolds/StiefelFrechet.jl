b90f73b170260abec9d54793bd7b94e0df753a32
dnguyend@gmail.com <dnguyend@gmail.com>
2022-12-03 20:22:22 -0500

    fix default values

--- src/manifolds/StiefelFrechet.jl
+++ src/manifolds/StiefelFrechet.jl
@@ -86,6 +86,7 @@ The exact gradient calculation with Fréchet derivatives is relatively expensive
     + ``c1`` (default = 1e-4)  default c1, c2 in the line search.\
     + ``c2`` (default = 0.9).\
     +  ``max\_ls`` (default = 25) max iterations for the line search.
+    + ``max_fun_evals`` (default = Int(ceil(max_itr * 1.3))) max function evaluations
 """
 function log_lbfgs(
     Stf,
@@ -97,18 +98,21 @@ function log_lbfgs(
     lbfgs_options=nothing,
 )
     # these are default values - overide in lbfgs_options.
+    
     if !isnothing(lbfgs_options)
         complementary_rank_cutoff = get(lbfgs_options, "complementary_rank_cutoff", 1e-14)
         corrections = get(lbfgs_options, "corrections", 10)
         c1 = get(lbfgs_options, "c1", 1e-4)
         c2 = get(lbfgs_options, "c2", .9)
         max_ls = get(lbfgs_options, "max_ls", 25)
+        max_fun_evals = get(lbfgs_options, "max_fun_evals", Int(ceil(max_itr * 1.3)))
     else
         complementary_rank_cutoff = 1e-14
         corrections = 10
         c1 = 1e-4
         c2 = .9
         max_ls = 25
+        max_fun_evals = Int(ceil(max_itr * 1.3))
     end
 
     α = metric(Stf).α
