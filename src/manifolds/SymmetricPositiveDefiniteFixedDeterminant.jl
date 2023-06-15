99d8cfc80814560e0f334b8c50dc586630641a40
Ronny Bergmann <git@ronnybergmann.net>
2023-06-15 13:42:57 +0200

    initial sketch of SPD fixed det.

---
+++ src/manifolds/SymmetricPositiveDefiniteFixedDeterminant.jl
@@ -0,0 +1,106 @@
+@doc raw"""
+    SymmetricPositiveDefiniteFixedRank{N,D} <: AbstractDecoratorManifold{ℝ}
+
+The manifold of symmetric positive definite matrices of fixed determinant ``d > 0``, i.e.
+
+````math
+\mathcal P_d(n) =
+\bigl\{
+p ∈ ℝ^{n × n} \ \big|\ a^\mathrm{T}pa > 0 \text{ for all } a ∈ ℝ^{n}\backslash\{0\}
+  \text{ and } \det(p) = d
+\bigr\}.
+````
+This manifold is modelled as a submanifold of [`SymmetricPositiveDefinite`](@ref)`(n)`.
+
+The tangent space is modelled the same as for [`SymmetricPositiveDefinite`](@ref)`(n)`
+and consists of all symmetric matrices with zero trace
+```math
+    T_p\mathcal P_d(n) =
+    \bigl\{
+        X \in \mathbb R^{n×n} \big|\ X=X^\mathrm{T} \text{ and } \operatorname{tr}(p) = 0
+    \bigr\},
+```
+since for a constant determinant we require that `0 = D\det(p)[Z] = \det(p)\operatorname{tr}(p^{-1}Z)` for all tangent vectprs ``Z``.
+Additionally we store the tangent vectors as `X=p^{-1}Z`, i.e. symmetric matrices.
+
+# Constructor
+
+    SymmetricPositiveDefiniteFixedDeterminant(n, d=1.0)
+
+generates the manifold $\mathcal P_d(n) \subset \mathcal P(n)$ of determinant ``d``,
+which defaults to 1.
+"""
+
+struct SymmetricPositiveDefiniteFixedDeterminant{N,D} <: AbstractDecoratorManifold{ℝ} end
+
+function SymmetricPositiveDefiniteFixedDeterminant(n::Int, d::F=1.0) where {F <: Real}
+    @assert d > 0 "The determinant has to be positive but was provided as $d."
+    return SymmetricPositiveDefiniteFixedDeterminant{n,d}()
+end
+
+function active_traits(f, ::SymmetricPositiveDefiniteFixedDeterminant, args...)
+    return merge_traits(IsEmbeddedSubmanifold())
+end
+
+@doc raw"""
+    check_point(M::SymmetricPositiveDefiniteFixedDeterminant{n,d}, p; kwargs...)
+
+Check whether `p` is a valid manifold point on the [`SymmetricPositiveDefiniteFixedDeterminant`](@ref)`(n,d)` `M`, i.e.
+whether `p` is a [`SymmetricPositiceDefinite`](@ref) matrix (checked by traits) ofsize `(n,n)`
+
+with determinant ``\det(p) = d``.
+
+The tolerance for the determinant of `p` can be set using `kwargs...`.
+"""
+function check_point(
+    M::SymmetricPositiveDefiniteFixedDeterminant{n,d},
+    p;
+    kwargs...,
+) where {n,d}
+    if det(p) != d
+        return DomainError(
+            det(p),
+            "The point $(p) does not lie on $M, since it does not have a determinant $d.",
+        )
+    end
+    return nothing
+end
+
+"""
+    check_vector(M::SymmetricMatrices{n,𝔽}, p, X; kwargs... )
+
+Check whether `X` is a tangent vector to manifold point `p` on the
+[`SymmetricMatrices`](@ref) `M`, i.e. `X` has to be a symmetric matrix of size `(n,n)`
+and its values have to be from the correct [`AbstractNumbers`](https://juliamanifolds.github.io/ManifoldsBase.jl/stable/types.html#number-system).
+
+The tolerance for the symmetry of `X` can be set using `kwargs...`.
+"""
+function check_vector(M::SymmetricMatrices{n,𝔽}, p, X; kwargs...) where {n,𝔽}
+    if !isapprox(norm(X - X'), 0.0; kwargs...)
+        return DomainError(
+            norm(X - X'),
+            "The vector $(X) is not a tangent vector to $(p) on $(M), since it is not symmetric.",
+        )
+    end
+    return nothing
+end
+
+embed(::SymmetricPositiveDefiniteFixedDeterminant, p) = p
+embed(::SymmetricPositiveDefiniteFixedDeterminant, p, X) = X
+embed!(::SymmetricPositiveDefiniteFixedDeterminant, q, p) = copyto!(M, q, p)
+embed!(::SymmetricPositiveDefiniteFixedDeterminant, Y, p, X) = copyto!(M; y, p, X)
+
+function get_embedding(::SymmetricPositiveDefiniteFixedDeterminant{n,d}) where {n,d}
+    return SymmetricPositiveDefinite(n)
+end
+
+function project!(::SymmetricPositiveDefiniteFixedDeterminant{n,d}, q, p) where {n,d}
+    q .= (d / det(p))^1 / n .* p
+    return
+end
+
+function project!(M::SymmetricPositiveDefiniteFixedDeterminant, Y, p, X)
+    copyto!(M, Y, p, X)
+    Y[diagind(Y)] .= 0.0
+    return Y
+end
