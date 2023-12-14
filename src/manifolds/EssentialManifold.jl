8b57a5b1e22e8584e4243a4daa064e2972a1450a
Ronny Bergmann <git@ronnybergmann.net>
2023-12-14 16:39:22 +0100

    Fix essential manifold.

--- src/manifolds/EssentialManifold.jl
+++ src/manifolds/EssentialManifold.jl
@@ -125,7 +125,13 @@ exp(::EssentialManifold, ::Any...)
 
 get_iterator(::EssentialManifold) = Base.OneTo(2)
 
-function _isapprox(M::EssentialManifold, p, q; atol=eps(float(eltype(p))), kwargs...)
+function _isapprox(
+    M::EssentialManifold,
+    p,
+    q::T;
+    atol=eps(real(float(number_eltype(number_eltype(T))))),
+    kwargs...,
+) where {T}
     return isapprox(distance(M, p, q), 0.0; atol=atol, kwargs...)
 end
 
