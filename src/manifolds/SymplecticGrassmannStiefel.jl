6eb0d3eabfe1023e198c620717254606b486092b
Ronny Bergmann <git@ronnybergmann.net>
2024-01-25 08:25:23 +0100

    Rephrase a docstring.

--- src/manifolds/SymplecticGrassmannStiefel.jl
+++ src/manifolds/SymplecticGrassmannStiefel.jl
@@ -65,15 +65,22 @@ end
 @doc raw"""
     inner(::SymplecticGrassmann, p, X, Y)
 
-Compute the Riemannian inner product ``g^{\mathrm{SpGr}}_p(X,Y)``, where ``p``
-is a point on the [`SymplecticStiefel`](@ref) manifold and ``X,Y \in \mathrm{Hor}_p^π\operatorname{SpSt}(2n,2k)``
-are horizontal tangent vectors. The formula reads according to Proposition Lemma 4.8 [BendokatZimmermann:2021](@cite).
+Compute the Riemannian inner product ``g^{\mathrm{SpGr}}_p(X,Y)``
+on the [`SymplecticGrassmann`](@ref) manifold `\mathrm{SpGr}``.
+
+For the case where ``p`` is represented by a point on the [`SymplecticStiefel`](@ref) manifold
+acting as a representant of its equivalence class ``[p] \in \mathrm{SpGr}``
+and the tangent vectors ``X,Y \in \mathrm{Hor}_p^π\operatorname{SpSt}(2n,2k)``
+are horizontal tangent vectors.
+
+Then the inner product reads according to Proposition Lemma 4.8 [BendokatZimmermann:2021](@cite).
 
 ```math
 g^{\mathrm{SpGr}}_p(X,Y) = \operatorname{tr}\bigl(
         (p^{\mathrm{T}}p)^{-1}X^{\mathrm{T}}(I_{2n} - pp^+)Y
     \bigr),
 ```
+
 where ``I_{2n}`` denotes the identity matrix and ``(⋅)^+`` the [`symplectic_inverse`](@ref).
 """
 function inner(M::SymplecticGrassmann, p, X, Y)
