36a00d18f5941d596ac67ae2b47db2a6d9171676
Ronny Bergmann <git@ronnybergmann.net>
2024-05-07 11:19:22 +0200

    Update src/manifolds/StiefelEuclideanMetric.jl
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/manifolds/StiefelEuclideanMetric.jl
+++ src/manifolds/StiefelEuclideanMetric.jl
@@ -103,7 +103,7 @@ end
 _euclidean_unit_vector(n, i) = [k == i ? 1.0 : 0.0 for k in 1:n]
 
 @doc raw"""
-    injectivity_radius(M::Stiefel[, p])
+    injectivity_radius(M::Stiefel)
 
 Return the injectivity radius for the [`Stiefel`](@ref) manifold `M`,
 which is globally ``π`` [ZimmermannStoye:2024](@cite).
