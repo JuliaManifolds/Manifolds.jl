fd1ae91b5b944b3bf5fdd54fb832cf7bf00f9715
Mateusz Baran <mateuszbaran89@gmail.com>
2024-05-26 11:31:12 +0200

    be more informative when a Nothing is passed instead of a module

--- docs/make.jl
+++ docs/make.jl
@@ -68,6 +68,28 @@ bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"); style=:a
 links = InterLinks(
     "ManifoldsBase" => ("https://juliamanifolds.github.io/ManifoldsBase.jl/stable/"),
 )
+modules = [
+    Manifolds,
+    isdefined(Base, :get_extension) ?
+    Base.get_extension(Manifolds, :ManifoldsBoundaryValueDiffEqExt) :
+    Manifolds.ManifoldsBoundaryValueDiffEqExt,
+    isdefined(Base, :get_extension) ?
+    Base.get_extension(Manifolds, :ManifoldsNLsolveExt) : Manifolds.ManifoldsNLsolveExt,
+    isdefined(Base, :get_extension) ?
+    Base.get_extension(Manifolds, :ManifoldsOrdinaryDiffEqDiffEqCallbacksExt) :
+    Manifolds.ManifoldsOrdinaryDiffEqDiffEqCallbacksExt,
+    isdefined(Base, :get_extension) ?
+    Base.get_extension(Manifolds, :ManifoldsOrdinaryDiffEqExt) :
+    Manifolds.ManifoldsOrdinaryDiffEqExt,
+    isdefined(Base, :get_extension) ?
+    Base.get_extension(Manifolds, :ManifoldsRecipesBaseExt) :
+    Manifolds.ManifoldsRecipesBaseExt,
+    isdefined(Base, :get_extension) ? Base.get_extension(Manifolds, :ManifoldsTestExt) :
+    Manifolds.ManifoldsTestExt,
+]
+if modules isa Vector{Union{Nothing,Module}}
+    error("At least one module has not been properly loaded: ", modules)
+end
 makedocs(;
     # for development, we disable prettyurls
     format=Documenter.HTML(
@@ -76,25 +98,7 @@ makedocs(;
         size_threshold_warn=200 * 2^10, # raise slightly from 100 to 200 KiB
         size_threshold=300 * 2^10,      # raise slightly 200 to to 300 KiB
     ),
-    modules=[
-        Manifolds,
-        isdefined(Base, :get_extension) ?
-        Base.get_extension(Manifolds, :ManifoldsBoundaryValueDiffEqExt) :
-        Manifolds.ManifoldsBoundaryValueDiffEqExt,
-        isdefined(Base, :get_extension) ?
-        Base.get_extension(Manifolds, :ManifoldsNLsolveExt) : Manifolds.ManifoldsNLsolveExt,
-        isdefined(Base, :get_extension) ?
-        Base.get_extension(Manifolds, :ManifoldsOrdinaryDiffEqDiffEqCallbacksExt) :
-        Manifolds.ManifoldsOrdinaryDiffEqDiffEqCallbacksExt,
-        isdefined(Base, :get_extension) ?
-        Base.get_extension(Manifolds, :ManifoldsOrdinaryDiffEqExt) :
-        Manifolds.ManifoldsOrdinaryDiffEqExt,
-        isdefined(Base, :get_extension) ?
-        Base.get_extension(Manifolds, :ManifoldsRecipesBaseExt) :
-        Manifolds.ManifoldsRecipesBaseExt,
-        isdefined(Base, :get_extension) ? Base.get_extension(Manifolds, :ManifoldsTestExt) :
-        Manifolds.ManifoldsTestExt,
-    ],
+    modules=modules,
     authors="Seth Axen, Mateusz Baran, Ronny Bergmann, and contributors.",
     sitename="Manifolds.jl",
     pages=[
