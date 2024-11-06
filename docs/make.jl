eefa4a66d4fcf12c83ab0b74d152b6839263ae6e
Ronny Bergmann <git@ronnybergmann.net>
2024-11-06 13:46:40 +0100

    Update make.jl to allow to exclude tutorial rendering.

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,32 @@
 #
 #
 
+if "--help" ∈ ARGS
+    println(
+        """
+docs/make.jl
+
+Render the `Manopt.jl` documentation with optional arguments
+
+Arguments
+* `--exclude-tutorials` - exclude the tutorials from the menu of Documenter,
+  this can be used if you do not have Quarto installed to still be able to render the docs
+  locally on this machine. This option should not be set on CI.
+* `--help`              - print this help and exit without rendering the documentation
+* `--prettyurls`        – toggle the prettyurls part to true (which is otherwise only true on CI)
+* `--quarto`            – run the Quarto notebooks from the `tutorials/` folder before generating the documentation
+  this has to be run locally at least once for the `tutorials/*.md` files to exist that are included in
+  the documentation (see `--exclude-tutorials`) for the alternative.
+  If they are generated once they are cached accordingly.
+  Then you can spare time in the rendering by not passing this argument.
+  If quarto is not run, some tutorials are generated as empty files, since they
+  are referenced from within the documentation. These are currently
+  `Optimize.md` and `ImplementOwnManifold.md`.
+""",
+    )
+    exit(0)
+end
+
 #
 # (a) if docs is not the current active environment, switch to it
 # (from https://github.com/JuliaIO/HDF5.jl/pull/1020/) 
@@ -11,12 +37,9 @@ if Base.active_project() != joinpath(@__DIR__, "Project.toml")
     Pkg.develop(PackageSpec(; path=(@__DIR__) * "/../"))
     Pkg.resolve()
     Pkg.instantiate()
-    if "--quarto" ∈ ARGS
-        Pkg.build("IJulia") # to activate the right kernel
-    end
 end
 
-# (b) Did someone say render? Then we render!
+# (b) Did someone say render?
 if "--quarto" ∈ ARGS
     using CondaPkg
     CondaPkg.withenv() do
@@ -24,13 +47,28 @@ if "--quarto" ∈ ARGS
         tutorials_folder = (@__DIR__) * "/../tutorials"
         # instantiate the tutorials environment if necessary
         Pkg.activate(tutorials_folder)
+        # For a breaking release -> also set the tutorials folder to the most recent version
         Pkg.develop(PackageSpec(; path=(@__DIR__) * "/../"))
         Pkg.resolve()
         Pkg.instantiate()
+        Pkg.build("IJulia") # build `IJulia` to the right version.
         Pkg.activate(@__DIR__) # but return to the docs one before
         run(`quarto render $(tutorials_folder)`)
         return nothing
     end
+else # fallback to at least create empty files for the start tutorial since that is linked
+    touch(joinpath(@__DIR__, "src/tutorials/getstarted.md"))
+end
+
+tutorials_in_menu = true
+if "--exclude-tutorials" ∈ ARGS
+    @warn """
+    You are excluding the tutorials from the Menu,
+    which might be done if you can not render them locally.
+
+    Remember that this should never be done on CI for the full documentation.
+    """
+    tutorials_in_menu = false
 end
 
 # (c) load necessary packages for the docs
@@ -65,7 +103,18 @@ for fname in ["CONTRIBUTING.md", "NEWS.md"]
     end
 end
 
-# (e) ...finally! make docs
+# (e) build the tutorials menu
+tutorials_menu =
+    "How to..." => [
+        "🚀 Get Started with `Manifolds.jl`" => "tutorials/getstarted.md",
+        "work in charts" => "tutorials/working-in-charts.md",
+        "perform Hand gesture analysis" => "tutorials/hand-gestures.md",
+        "integrate on manifolds and handle probability densities" => "tutorials/integration.md",
+        "explore curvature without coordinates" => "tutorials/exploring-curvature.md",
+        "work with groups" => "tutorials/groups.md",
+    ]
+# (f) final step: render the docs
+
 bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"); style=:alpha)
 links = InterLinks(
     "ManifoldsBase" => ("https://juliamanifolds.github.io/ManifoldsBase.jl/stable/"),
@@ -93,26 +142,18 @@ if modules isa Vector{Union{Nothing,Module}}
     error("At least one module has not been properly loaded: ", modules)
 end
 makedocs(;
-    # for development, we disable prettyurls
     format=Documenter.HTML(
-        prettyurls=false,
+        prettyurls=(get(ENV, "CI", nothing) == "true") || ("--prettyurls" ∈ ARGS),
         assets=["assets/favicon.ico", "assets/citations.css"],
         size_threshold_warn=200 * 2^10, # raise slightly from 100 to 200 KiB
-        size_threshold=300 * 2^10,      # raise slightly 200 to to 300 KiB
+        size_threshold=300 * 2^10,      # raise slightly 200 to 300 KiB
     ),
     modules=modules,
     authors="Seth Axen, Mateusz Baran, Ronny Bergmann, and contributors.",
     sitename="Manifolds.jl",
     pages=[
         "Home" => "index.md",
-        "How to..." => [
-            "🚀 Get Started with `Manifolds.jl`" => "tutorials/getstarted.md",
-            "work in charts" => "tutorials/working-in-charts.md",
-            "perform Hand gesture analysis" => "tutorials/hand-gestures.md",
-            "integrate on manifolds and handle probability densities" => "tutorials/integration.md",
-            "explore curvature without coordinates" => "tutorials/exploring-curvature.md",
-            "work with groups" => "tutorials/groups.md",
-        ],
+        (tutorials_in_menu ? [tutorials_menu] : [])...,
         "Manifolds" => [
             "Basic manifolds" => [
                 "Centered matrices" => "manifolds/centeredmatrices.md",
