b4394a9cf325a950a1e7a49fb1079692888c0910
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 18:13:49 +0100

    Format

--- ext/ManifoldsRecursiveArrayToolsExt/ManifoldsRecursiveArrayToolsExt.jl
+++ ext/ManifoldsRecursiveArrayToolsExt/ManifoldsRecursiveArrayToolsExt.jl
@@ -1,61 +1,61 @@
 module ManifoldsRecursiveArrayToolsExt
 
-    using Manifolds
-    using RecursiveArrayTools: ArrayPartition
-    using StaticArrays
-    using LinearAlgebra
+using Manifolds
+using RecursiveArrayTools: ArrayPartition
+using StaticArrays
+using LinearAlgebra
 
-    using Base.Iterators: repeated
+using Base.Iterators: repeated
 
-    using Manifolds:
-        ActionDirectionAndSide,
-        ColumnwiseMultiplicationAction,
-        FiberBundleBasisData,
-        FiberBundleProductVectorTransport,
-        LeftColumnwiseSpecialEuclideanAction,
-        LeftInvariantRepresentation,
-        PowerManifoldNestedReplacing,
-        SpecialEuclideanIdentity,
-        SpecialEuclideanInGeneralLinear,
-        TangentVectorRepresentation,
-        TypeParameter
+using Manifolds:
+    ActionDirectionAndSide,
+    ColumnwiseMultiplicationAction,
+    FiberBundleBasisData,
+    FiberBundleProductVectorTransport,
+    LeftColumnwiseSpecialEuclideanAction,
+    LeftInvariantRepresentation,
+    PowerManifoldNestedReplacing,
+    SpecialEuclideanIdentity,
+    SpecialEuclideanInGeneralLinear,
+    TangentVectorRepresentation,
+    TypeParameter
 
-    using Manifolds:
-        bundle_transport_tangent_direction, bundle_transport_tangent_to, _get_parameter
+using Manifolds:
+    bundle_transport_tangent_direction, bundle_transport_tangent_to, _get_parameter
 
-    import Manifolds:
-        adjoint_Jacobi_field,
-        allocate,
-        allocate_result,
-        apply,
-        apply!,
-        apply_diff,
-        apply_diff!,
-        apply_diff_group!,
-        _common_product_translate_diff,
-        compose,
-        _compose,
-        exp_lie,
-        get_coordinates,
-        get_vector,
-        get_vectors,
-        hat,
-        identity_element,
-        inverse_apply,
-        inverse_apply_diff,
-        inverse_translate,
-        inverse_translate_diff,
-        isapprox,
-        jacobi_field,
-        lie_bracket,
-        optimal_alignment,
-        project,
-        translate,
-        translate_diff,
-        vector_representation,
-        _vector_transport_direction,
-        _vector_transport_to,
-        vee
+import Manifolds:
+    adjoint_Jacobi_field,
+    allocate,
+    allocate_result,
+    apply,
+    apply!,
+    apply_diff,
+    apply_diff!,
+    apply_diff_group!,
+    _common_product_translate_diff,
+    compose,
+    _compose,
+    exp_lie,
+    get_coordinates,
+    get_vector,
+    get_vectors,
+    hat,
+    identity_element,
+    inverse_apply,
+    inverse_apply_diff,
+    inverse_translate,
+    inverse_translate_diff,
+    isapprox,
+    jacobi_field,
+    lie_bracket,
+    optimal_alignment,
+    project,
+    translate,
+    translate_diff,
+    vector_representation,
+    _vector_transport_direction,
+    _vector_transport_to,
+    vee
 
 function allocate(
     ::PowerManifoldNestedReplacing,
