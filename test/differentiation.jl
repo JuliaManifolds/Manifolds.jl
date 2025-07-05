5be8e4a7a0b523f552c9e960aa24737553829b2c
Ronny Bergmann <git@ronnybergmann.net>
2025-07-05 12:29:24 +0200

    Update test/differentiation.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/differentiation.jl
+++ test/differentiation.jl
@@ -208,7 +208,10 @@ end
     f1(p) = distance(s2, q2, p)^2
 
     # ManifoldDiff currently NaNs here, we should check why
-    @test_broken isapprox(ManifoldDiff.hessian(s2, f1, q, rb_onb_fwd_diff), [2.0 0.0; 0.0 0.0])
+    @test_broken isapprox(
+        ManifoldDiff.hessian(s2, f1, q, rb_onb_fwd_diff),
+        [2.0 0.0; 0.0 0.0],
+    )
 end
 
 @testset "EmbeddedBackend" begin
