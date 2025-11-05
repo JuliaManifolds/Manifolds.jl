63a5abcc3553b888653ccb41fd473dbfd12a9b6a
Ronny Bergmann <git@ronnybergmann.net>
2025-11-05 19:08:41 +0100

    add all functions as a todo list.

--- src/test.jl
+++ src/test.jl
@@ -20,7 +20,41 @@ function test_manifold end
 #
 #
 # the small functions per single API function
-function test_exp end
+function test_exp end # TODO: Test fused?
 function test_log end
 function test_manifold_dimension end
+#
+#
+# TODO
+function test_copy end
+function test_copyto end
+function test_default_atlas end
+function test_default_retraction end
+function test_default_inverse_retraction end
+function test_default_vector_transport end
+function test_distance end
+function test_embed end
+function test_flat end
+function test_geodesic end
+function test_get_basis end
+function test_get_coordinates end
+function test_get_vector end
+function test_get_vectors end
+function test_injectivity_radius end
+function test_inner end
+function test_inverse_retraction end
+function test_is_point end
+function test_is_vector end
+function test_mid_point end
+function test_norm end
+function test_parallel_transport end
+function test_project end
+function test_project_embed end
+function test_rand end
+function test_repr end
+function test_retraction end
+function test_sharp end
+function test_shortest_geodesic end
+function test_vector_transport end
+function test_zero_vector end
 end
