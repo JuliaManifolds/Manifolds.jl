fc1ed423e9d0364af1c0c725967be4158b42a2bc
Ronny Bergmann <git@ronnybergmann.net>
2025-11-05 15:26:14 +0100

    Towards a test suite

--- ext/ManifoldsTestExt/ManifoldsTestExt.jl
+++ ext/ManifoldsTestExt/ManifoldsTestExt.jl
@@ -4,57 +4,53 @@ using Manifolds
 using ManifoldsBase
 using Test
 
-function __init__()
-    # Slightly hacked, since we can not extend submodules for now, discuss, what is preferrable
-    setglobal!(Manifolds, :Test, Test)
-    return nothing
-end
+"""
+    text_exp(
+        M, p, X;
+        available_functions=[], expected_value=nothing, test_mutating=true,
+        test_log = (log in available_functions),
+        test_injectivity_radius = (injectivity_radius in available_functions),
+        kwargs...
+    )
+    Test the exponential map on manifold `M` at point `p` with tangent vector `X`.
 
-module Test
-    using Manifolds, Test, ManifoldsBase
-
-    """
-        text_exp(
-            M, p, X;
-            available_functions=[], expected_value=nothing, test_mutating=true, kwargs...
-        )
-        Test the exponential map on manifold `M` at point `p` with tangent vector `X`.
-
-        * that the result is a valid point on the manifold
-        * that the result matches `expected_value`, if given
-        * that the mutating version `exp!` matches the non-mutating version, if activated
-        * that the logarithmic map inverts the exponential map, if `log` is available
-          (only performed if either `injectivity_radius` is not available or `X` is within)
-    """
-    function test_exp(
-            M::AbstractManifold, p, X;
-            available_functions = Function[],
-            expected_value = nothing,
-            test_mutating = true,
-            kwargs...
-        )
-        @testset "Exponential map on $M with point $(typeof(p))" begin
-            q = exp(M, p, X)
-            @test is_point(M, q; error = :error, kwargs...)
-            if expected_value !== nothing
-                @test isapprox(M, q, expected_value; error = :error, kwargs...)
-            end
-            if test_mutating
-                q2 = allocate(q)
-                exp!(M, q2, p, X)
-                @test isapprox(M, q2, q; error = :error, kwargs...)
-            end
-            if log in available_functions
-                # Test only if inj is not available of X is within inj radius
-                if !(injectivity_radius in available_functions) || norm(M, p, X) ≤ injectivity_radius(M, p)
-                    Y = log(M, p, q)
-                    @test isapprox(M, X, Y; error = :error, kwargs...)
-                end
-            end
+    * that the result is a valid point on the manifold
+    * that the result matches `expected_value`, if given
+    * that the mutating version `exp!` matches the non-mutating version, (if activated)
+    * that the logarithmic map inverts the exponential map (if activated)
+      (only performed if either `injectivity_radius` is not available or `X` is within)
+"""
+function Manifolds.Test.test_exp(
+        M::AbstractManifold, p, X;
+        available_functions = Function[],
+        expected_value = nothing,
+        test_log = (log in available_functions),
+        test_injectivity_radius = (injectivity_radius in available_functions),
+        test_mutating = true,
+        kwargs...
+    )
+    @testset "Exponential map on $M with point $(typeof(p))" begin
+        q = exp(M, p, X)
+        @test is_point(M, q; error = :error, kwargs...)
+        if expected_value !== nothing
+            @test isapprox(M, q, expected_value; error = :error, kwargs...)
         end
-        return nothing
-    end # Manifolds.Test.test_exp
-end
+        if test_mutating
+            q2 = copy(M, p)
+            exp!(M, q2, p, X)
+            @test isapprox(M, q2, q; error = :error, kwargs...)
+        end
+        if test_log
+            # Test only if inj is not available of X is within inj radius
+            run_test = !test_injectivity_radius || norm(M, p, X) ≤ injectivity_radius(M, p)
+            run_test || (@warn("Skipping log-exp test since norm of X ($(norm(M, p, X))) is outside injectivity radius ($(injectivity_radius(M, p)))"); nothing)
+            Y = log(M, p, q)
+            @test isapprox(M, X, Y; error = :error, kwargs...) skip = !run_test
+        end
+    end
+    return nothing
+end # Manifolds.Test.test_exp
+
 # old includes
 include("tests_general.jl")
 
---
+++ src/test.jl
@@ -0,0 +1,17 @@
+"""
+    Manifolds.Test
+
+The module `Manifolds.Test` contains functions to test functions from `Manifolds.jl`.
+Most functionality is provided only when `Test.jl` is loaded as well, i.e. populated
+with methods by the extension.
+
+The test functions provided are mean to verify that the defined functions
+on a specific manifold behave as expected, for example that
+an allocating and in-place version of a function give the same result,
+or that for example the exponential map computes a point on the manifold,
+and calling the logarithmic map on the result yields the original tangent vector again,
+for tangent vectors within the injectivity radius.
+"""
+module Test
+function test_exp end
+end
