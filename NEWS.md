fa70e5b1462ad29b65dc77bb75dd87606c5c5b96
Mateusz Baran <mateuszbaran89@gmail.com>
2025-12-08 13:40:38 +0100

    Move MetricManifold and ConnectionManifold to ManifoldsBase.jl

--- NEWS.md
+++ NEWS.md
@@ -5,6 +5,12 @@ All notable changes to ´Manifolds.jl´ will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.11.8] 2025-12-09
+
+### Changed
+
+* `MetricManifold` and `ConnectionManifold` were moved to `ManifoldsBase.jl`.
+
 ## [0.11.7] 2025-11-27
 
 ### Added
--- src/manifolds/ConnectionManifold.jl
+++ src/manifolds/ConnectionManifold.jl
@@ -1,33 +1,4 @@
 @doc raw"""
-    AbstractAffineConnection
-
-Abstract type for affine connections on a manifold.
-"""
-abstract type AbstractAffineConnection end
-
-"""
-    LeviCivitaConnection
-
-The [Levi-Civita connection](https://en.wikipedia.org/wiki/Levi-Civita_connection) of a Riemannian manifold.
-"""
-struct LeviCivitaConnection <: AbstractAffineConnection end
-
-"""
-    ConnectionManifold{𝔽,,M<:AbstractManifold{𝔽},G<:AbstractAffineConnection} <: AbstractDecoratorManifold{𝔽}
-
-# Constructor
-
-    ConnectionManifold(M, C)
-
-Decorate the [`AbstractManifold`](@extref `ManifoldsBase.AbstractManifold`)  `M` with [`AbstractAffineConnection`](@ref) `C`.
-"""
-struct ConnectionManifold{𝔽, M <: AbstractManifold{𝔽}, C <: AbstractAffineConnection} <:
-    AbstractDecoratorManifold{𝔽}
-    manifold::M
-    connection::C
-end
-
-@doc raw"""
     christoffel_symbols_first(
         M::AbstractManifold,
         p,
@@ -149,42 +120,6 @@ end
 )
 
 """
-    connection(M::AbstractManifold)
-
-Get the connection (an object of a subtype of [`AbstractAffineConnection`](@ref))
-of [`AbstractManifold`](@extref `ManifoldsBase.AbstractManifold`) `M`.
-
-The global default connection is the [`LeviCivitaConnection`](@ref).
-"""
-connection(::AbstractManifold) = LeviCivitaConnection()
-
-"""
-    connection(M::ConnectionManifold)
-
-Return the connection associated with [`ConnectionManifold`](@ref) `M`.
-"""
-connection(M::ConnectionManifold) = M.connection
-
-decorated_manifold(M::ConnectionManifold) = M.manifold
-
-default_retraction_method(M::ConnectionManifold) = default_retraction_method(M.manifold)
-function default_retraction_method(M::ConnectionManifold, t::Type)
-    return default_retraction_method(M.manifold, t)
-end
-function default_inverse_retraction_method(M::ConnectionManifold)
-    return default_inverse_retraction_method(M.manifold)
-end
-function default_inverse_retraction_method(M::ConnectionManifold, t::Type)
-    return default_inverse_retraction_method(M.manifold, t)
-end
-function default_vector_transport_method(M::ConnectionManifold)
-    return default_vector_transport_method(M.manifold)
-end
-function default_vector_transport_method(M::ConnectionManifold, t::Type)
-    return default_vector_transport_method(M.manifold, t)
-end
-
-"""
     gaussian_curvature(M::AbstractManifold, p, B::AbstractBasis; backend::AbstractDiffBackend = default_differential_backend())
 
 Compute the Gaussian curvature of the manifold `M` at the point `p` using basis `B`.
@@ -200,19 +135,6 @@ end
     B::AbstractBasis;
     kwargs...,
 )
-
-"""
-    is_default_connection(M::AbstractManifold, c::AbstractAffineConnection)
-
-returns whether an [`AbstractAffineConnection`](@ref) is the default metric on the manifold `M` or not.
-
-This function falls back to check whether [`connection`](@ref)`(M) == c`.
-"""
-is_default_connection(M::AbstractManifold, c::AbstractAffineConnection)
-function is_default_connection(M::AbstractManifold, c::AbstractAffineConnection)
-    return connection(M) == c
-end
-is_default_connection(M::ConnectionManifold) = true
 
 """
     ricci_tensor(M::AbstractManifold, p, B::AbstractBasis; backend::AbstractDiffBackend = default_differential_backend())
@@ -287,7 +209,7 @@ function solve_exp_ode end
         kwargs...,
     )
 
-Approximate the exponential map on the manifold by evaluating the ODE descripting the geodesic at 1,
+Approximate the exponential map on the manifold by evaluating the ODE describing the geodesic at 1,
 assuming the default connection of the given manifold by solving the ordinary differential
 equation
 
