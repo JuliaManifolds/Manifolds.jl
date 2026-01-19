9477eca7771141b145fdf098093213a43ecc9f79
Ronny Bergmann <git@ronnybergmann.net>
2026-01-19 18:04:36 +0100

    Move determinant one matrices.

---
+++ test/manifolds/test_determinant_one_matrices.jl
@@ -0,0 +1,63 @@
+using LinearAlgebra, Manifolds, ManifoldsBase, Random, Test
+
+
+@testset "Matrices of Determinant 1" begin
+    # Real case
+    M = DeterminantOneMatrices(2)
+    p = [1.0 0.0; 0.0 1.0]
+    q = [10.0 0.0; 0.0 0.1]
+    pf = [2.0 0.0; 0.0 1.0]
+    qf = [1.0 0.0; 0.0 0.0]
+    X = [0.0 0.23; -0.14 0.0]
+    Y = [0.0 -0.5; 0.5 0.0]
+    Xf = [1.0 1.1; 1.2 0.0]
+
+    Manifolds.Test.test_manifold(
+        M,
+        Dict(
+            :Functions => [is_point, is_vector, project, embed],
+            :Points => [p, q], :Vectors => [X, Y],
+            :InvalidPoints => [pf, qf], :InvalidVectors => [Xf],
+            :EmbeddedPoints => [p], :EmbeddedVectors => [X],
+        ),
+        # Expectations
+        Dict(
+            :manifold_dimension => 3,
+            repr => "DeterminantOneMatrices(2, ?)",
+            get_embedding => Euclidean(2, 2),
+        ),
+    )
+    # Complex case
+    Mc = DeterminantOneMatrices(2, ?)
+    pc = [1.0im 0.0; 0.0 -1.0im]
+    qc = [10.0im 0.0; 0.0 -0.1im]
+    Xc = [0.0 0.23im; -0.14 0.0]
+    Yc = [0.0 -0.5im; 0.5im 0.0]
+    pcf = [10.0 0.0; 0.0 0.0]
+    qcf = [2.0 0.0; 0.0 1.0]
+    Xf = [1.0 + 2.0im 1.1 + 0.1im; 1.2 0.0]
+
+    Manifolds.Test.test_manifold(
+        Mc,
+        Dict(
+            :Functions => [is_point, is_vector, project, embed],
+            :Points => [pc, qc], :Vectors => [Xc, Yc],
+            :InvalidPoints => [pcf, qcf], :InvalidVectors => [Xf],
+            :EmbeddedPoints => [pc], :EmbeddedVectors => [Xc],
+        ),
+        # Expectations
+        Dict(
+            :manifold_dimension => 3,
+            repr => "DeterminantOneMatrices(2, ?)",
+            get_embedding => Euclidean(2, 2; field = ?),
+        ),
+    )
+
+
+    # Parameter
+    Manifolds.Test.test_manifold(
+        DeterminantOneMatrices(2; parameter = :field),
+        Dict(:Functions => [repr]),
+        Dict(repr => "DeterminantOneMatrices(2, ?; parameter=:field)"),
+    )
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -32,6 +32,7 @@ end
     end
     (TEST_SET ∈ ["all", "manifolds"]) && Test.@testset "Manifolds Test Suite" begin
         include_test("manifolds/test_centered_matrices.jl")
+        include_test("manifolds/test_determinant_one_matrices.jl")
         include_test("manifolds/test_cholesky_space.jl")
         include_test("manifolds/test_circle.jl")
         include_test("manifolds/test_sphere.jl")
@@ -40,7 +41,6 @@ end
     end
     (TEST_SET ∈ ["all", "manifolds"]) && Test.@testset "Manifolds.jl Old Tests" begin
         # starting with tests of simple manifolds
-        include_test("manifolds-old/determinant_one_matrices.jl")
         include_test("manifolds-old/elliptope.jl")
         include_test("manifolds-old/euclidean.jl")
         include_test("manifolds-old/fixed_rank.jl")
