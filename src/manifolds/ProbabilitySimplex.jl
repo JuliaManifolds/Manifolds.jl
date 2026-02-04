d1b8a5439e0e978e28807b060d5cb6c82cc21495
Mateusz Baran <mateuszbaran89@gmail.com>
2026-02-04 10:24:48 +0100

    we shouldn't need IJulia for tutorials; also, add a missing space

--- src/manifolds/ProbabilitySimplex.jl
+++ src/manifolds/ProbabilitySimplex.jl
@@ -224,7 +224,7 @@ end
     injectivity_radius(M::ProbabilitySimplex, p)
 
 Compute the injectivity radius on the [`ProbabilitySimplex`](@ref) `M` at the point `p`,
-i.e. the distanceradius to a point near/on the boundary, that could be reached by following the
+i.e. the distance radius to a point near/on the boundary, that could be reached by following the
 geodesic.
 """
 function injectivity_radius(::ProbabilitySimplex, p)
