e71b830703e5db4604bbbe5bbb0ad4086dacd548
Mateusz Baran <mateuszbaran89@gmail.com>
2026-03-31 12:32:07 +0200

    bring back more tests

--- test/manifolds-old/unitary_matrices.jl
+++ test/manifolds-old/unitary_matrices.jl
@@ -2,6 +2,69 @@ include("../header.jl")
 
 using Quaternions
 
+
+@testset "Orthogonal Matrices" begin
+    @test injectivity_radius(M, PolarRetraction()) == π / sqrt(2.0)
+    @test injectivity_radius(M) == π * sqrt(2.0)
+    @test injectivity_radius(OrthogonalMatrices(3; parameter = :field)) == π * sqrt(2.0)
+    @test injectivity_radius(OrthogonalMatrices(1; parameter = :field)) == 0.0
+    @test !is_flat(M)
+    p = project(M, ones(3, 3))
+    @test is_point(M, p; error = :error)
+    @test is_point(M, rand(M); error = :error)
+    @test is_vector(M, p, rand(M; vector_at = p))
+    @test is_point(M, rand(MersenneTwister(), M); error = :error)
+    @test is_vector(M, p, rand(MersenneTwister(), M; vector_at = p))
+    @test default_vector_transport_method(M) === ProjectionTransport()
+end
+
+@testset "Unitary Matrices" begin
+    M = UnitaryMatrices(2)
+    @test repr(M) == "UnitaryMatrices(2)"
+    @test repr(UnitaryMatrices(2; parameter = :field)) ==
+        "UnitaryMatrices(2; parameter=:field)"
+    @test manifold_dimension(M) == 4
+    @test !is_flat(M)
+    @test injectivity_radius(M) == π
+
+    # wrong length of size
+    @test_throws DomainError is_point(M, zeros(1); error = :error)
+    @test_throws DomainError is_point(M, zeros(3, 3); error = :error)
+    pF = 1 / 2 .* [1im 1im; -1im 1im]
+    @test_throws DomainError is_point(M, pF; error = :error)
+    # Determinant not one
+    pF2 = [1im 1.0; 0.0 -1im]
+    @test_throws DomainError is_point(M, pF2; error = :error)
+    p = [1im 0.0; 0.0 1im]
+    @test is_point(M, p; error = :error)
+
+    @test_throws DomainError is_vector(M, p, zeros(1); error = :error)
+    @test_throws DomainError is_vector(M, p, zeros(3, 3); error = :error)
+    # not skew
+    @test_throws DomainError is_vector(M, p, ones(2, 2); error = :error)
+    X = [0.0 1.0; -1.0 0.0]
+    @test is_vector(M, p, X; error = :error)
+
+    q = project(M, ones(2, 2))
+    @test is_point(M, q; error = :error)
+    q2 = project(M, 1im * ones(2, 2))
+    @test is_point(M, q2; error = :error)
+
+    r = exp(M, p, X)
+    X2 = log(M, p, r)
+    @test isapprox(M, p, X, X2)
+    r1 = Manifolds.exp_fused(M, p, X, 1.0)
+    @test isapprox(M, r, r1; atol = 1.0e-10)
+
+    @testset "Random" begin
+        M = UnitaryMatrices(2)
+        Random.seed!(23)
+        p = rand(M)
+        @test is_point(M, p; error = :error)
+        @test is_vector(M, p, rand(M; vector_at = p); error = :error)
+    end
+end
+
 @testset "Special unitary matrices" begin
     @test manifold_volume(SpecialUnitaryMatrices(1)) ≈ 1
     @test manifold_volume(SpecialUnitaryMatrices(2)) ≈ 2 * π^2
@@ -15,6 +78,33 @@ using Quaternions
     @test injectivity_radius(SpecialUnitaryMatrices(2)) == π * sqrt(2)
     @test injectivity_radius(SpecialUnitaryMatrices(3)) == π * sqrt(2)
     @test injectivity_radius(SpecialUnitaryMatrices(4)) == π * sqrt(2)
+end
+
+@testset "Quaternionic Unitary Matrices" begin
+    M = UnitaryMatrices(1, ℍ)
+    @testset "rand" begin
+        p = rand(M)
+        @test is_point(M, p)
+        X = rand(M; vector_at = p)
+        @test is_vector(M, p, X)
+        p = rand(MersenneTwister(), M)
+        @test is_point(M, p)
+        X = rand(MersenneTwister(), M; vector_at = p)
+        @test is_vector(M, p, X)
+    end
+
+    # wrong length of size
+    @test_throws DomainError is_point(M, zeros(2, 2); error = :error)
+
+    # Determinant not one
+    pF2 = [quat(0, 1, 0, 0) 1.0; 0.0 -quat(0, 1, 0, 0)]
+    @test_throws DomainError is_point(M, pF2; error = :error)
+    p = QuaternionF64(
+        0.4815296357756736,
+        0.6041613272484806,
+        -0.2322369798903669,
+        0.5909181717450419,
+    )
 end
 
 @testset "SO(4) and O(4) exp/log edge cases" begin
